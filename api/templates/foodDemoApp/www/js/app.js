fbe4a51778e3ea307f6a27165634d42e7e03ebd5
uruchira <uruchira@gmail.com>
2016-03-28 16:27:11 +0530

    Pages are structured.

--- api/templates/foodDemoApp/www/js/app.js
+++ api/templates/foodDemoApp/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('foodDemoApp', ['ionic', 'foodDemoApp.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -39,40 +39,57 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
   })
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.register', {
+      url: '/register',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/register.html'
+        }
+      }
+  })
+  .state('app.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html'
         }
       }
    })
-  .state('app.playlists', {
-    url: '/playlists',
+   .state('app.foods', {
+     url: '/foods',
+     views: {
+       'menuContent': {
+         templateUrl: 'templates/foods.html',
+         controller: 'FoodsCtrl'
+       }
+     }
+   })
+   .state('app.food', {
+     url: '/foods/:foodId',
+     views: {
+       'menuContent': {
+         templateUrl: 'templates/food.html',
+         controller: 'FoodCtrl'
+       }
+     }
+   })
+  .state('app.order', {
+    url: '/order',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlists.html',
-        controller: 'PlaylistsCtrl'
+        templateUrl: 'templates/order.html'
       }
     }
   })
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+  .state('app.cart', {
+    url: '/cart',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/cart.html'
       }
     }
   });
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/login');
 });
--- api/templates/foodDemoApp/www/js/controllers.js
+++ api/templates/foodDemoApp/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('foodDemoApp.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -6,51 +6,38 @@ angular.module('starter.controllers', [])
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
+  // $scope.$on('$ionicView.enter', function(e) {
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    //$scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    //$scope.modal.show();
-  };
+  // $scope.loginData = {};
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+
+  // $scope.doLogin = function() {
+    //console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+
+    // $timeout(function() {
+      // $scope.closeLogin();
+    // }, 1000);
+  // };
+
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('FoodsCtrl', function($scope) {
+  $scope.foods = [
+    { title: 'food 1', description: 'description 1', id: 1 },
+    { title: 'food 2', description: 'description 2', id: 2 },
+    { title: 'food 3', description: 'description 3', id: 3 },
+    { title: 'food 4', description: 'description 4', id: 4 },
+    { title: 'food 5', description: 'description 5', id: 5 },
+    { title: 'food 6', description: 'description 6', id: 6 }
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('FoodCtrl', function($scope, $stateParams) {
+  $scope.foodInfo = $stateParams.foodId;
 });
