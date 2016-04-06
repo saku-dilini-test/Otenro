6642b58ee5c204326cf34a1726efccdd7277e3ab
udeshika <udeshika@onbitlabs.com>
2016-04-06 16:44:49 +0530

    Bug fix - ME-321

--- assets/js/controllers/auth/loginController.js
+++ assets/js/controllers/auth/loginController.js
@@ -1,12 +1,21 @@
 (function() {
   'use strict';
+  angular.module('app').directive('preventDefault', function() {
+    return function(scope, element, attrs) {
+      angular.element(element).bind('submit', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      });
+    }
+  });
+
   angular.module('app')
       .controller('LoginController',['$scope','$state','Auth','toastr','$auth', LoginController]);
 
 
   function LoginController( $scope, $state, Auth ,toastr ,$auth) {
 
-    $scope.submit = function() {
+    $scope.submit = function($event) {
       Auth.login($scope.user).success(function() {
         toastr.success('Successfully login ', 'Message', {
           closeButton: true
@@ -20,7 +29,7 @@
     };
     $scope.cancel = function () {
       $state.go('user.welcome');
-    }
+    };
 
     $scope.authenticate = function(provider) {
       $auth.authenticate(provider);
