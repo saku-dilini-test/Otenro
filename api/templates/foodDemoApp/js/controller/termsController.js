f7089f344fe8d05b36901d732406e94d9ce60e43
amilaonbit <amilaonbit@gmail.com>
2016-04-28 17:17:44 +0530

    Food demo temple UI & function changes according given screenshot

---
+++ api/templates/foodDemoApp/js/controller/termsController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by amila on 4/28/16.
+ */
+
+mobileApp.controller('termsCtrl', function($scope) {
+
+    $scope.terms = "This terms content should come backend";
+    $scope.conditions = "This coditions content should come backend";
+
+});
