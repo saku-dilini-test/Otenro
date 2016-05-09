96d806a4017ddd8e3e165c98e47941b7d8e2797e
amilaonbit <amilaonbit@gmail.com>
2016-05-09 12:03:14 +0530

    fix google map loading issue

--- apps/TECCminify/js/app.js
+++ apps/TECCminify/js/app.js
@@ -1,4 +1,4 @@
-var cakeApp = angular.module('animateApp', ['ngRoute','satellizer']);
+var cakeApp = angular.module('animateApp', ['ngRoute','satellizer','wu.staticGmap']);
 
 //cakeApp.constant('SERVER_URL', "http://192.168.8.155:1339/");    // local development
 //cakeApp.constant('SERVER_URL', "http://192.168.8.155:1338/");  // local production
