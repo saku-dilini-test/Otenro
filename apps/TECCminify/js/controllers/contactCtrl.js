e5dff76678d05ba65e34a0cd5ea70f467e684202
amilaonbit <amilaonbit@gmail.com>
2016-05-12 09:18:57 +0530

    "Issues of TECC - Version 02" fix issues committed

--- apps/TECCminify/js/controllers/contactCtrl.js
+++ apps/TECCminify/js/controllers/contactCtrl.js
@@ -4,10 +4,11 @@ angular.module('animateApp')
     .controller('contactCtrl',['$scope','$http','SERVER_URL', function($scope,$http,SERVER_URL) {
 
         $scope.submitContactForm = function(data) {
-            console.log(data)
+
             $http.post(SERVER_URL+'contactUs/create', data)
                 .then(function(res){
-                    console.log(res);
+
+                    $scope.contactForm = '';
             });
         }
     }])
--- apps/cakeFactoryWebSite/js/controllers/contactCtrl.js
+++ apps/cakeFactoryWebSite/js/controllers/contactCtrl.js
@@ -4,10 +4,11 @@ angular.module('animateApp')
     .controller('contactCtrl', function($scope,$http,SERVER_URL) {
 
         $scope.submitContactForm = function(data) {
-            console.log(data)
+
             $http.post(SERVER_URL+'contactUs/create', data)
                 .then(function(res){
-                    console.log(res);
+
+                    $scope.contactForm = '';
             });
         }
     })
