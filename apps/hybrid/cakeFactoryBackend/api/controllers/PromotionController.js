815a9f23ec612bb66a16a3d30d8065dbc8ca54ee
shamila <shamila@onbitlabs.com>
2016-05-20 18:20:33 +0530

    test

--- apps/hybrid/cakeFactoryBackend/api/controllers/PromotionController.js
+++ apps/hybrid/cakeFactoryBackend/api/controllers/PromotionController.js
@@ -22,6 +22,6 @@ module.exports = {
   },
   paypalResponse : function(req,res){
           return res.json(200, {success: 'recieved'});
-          console.log("success "+ res.body);
-     }
+          console.log("successssssssssssssssss "+ res.body);
+  }
 };
