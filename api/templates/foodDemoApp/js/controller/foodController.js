01cf81c6c490c01ad4851de7299f8d2a6b550d04
prasanna <prasannabkmail@gmail.com>
2016-05-23 11:04:31 +0530

    bug fix ME-514

--- api/templates/foodDemoApp/js/controller/foodController.js
+++ api/templates/foodDemoApp/js/controller/foodController.js
@@ -15,6 +15,13 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
     }).error(function(err) {
         alert('warning', "Unable to get Products Selected Category", err.message);
     });
+    
+    $http.get(constants.SERVER_URL + '/templates/getCurrency?appId='+$scope.appId).success(function(data) {
+        $scope.currency = data;
+    }).error(function(err) {
+        alert('warning', "Unable to get Products Selected Category", err.message);
+    });
+    
 
     if($stateParams.foodId){
         $http.get(constants.SERVER_URL + '/templates/getProductById?productId='+$stateParams.foodId)
