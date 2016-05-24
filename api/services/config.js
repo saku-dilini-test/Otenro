ea612aeb0173c10689f4911319b6286c8088307d
amilaonbit <amilaonbit@gmail.com>
2016-05-24 10:43:06 +0530

    re commit issues

--- api/services/config.js
+++ api/services/config.js
@@ -1,12 +1,12 @@
 
 module.exports = {
 
-    ME_SERVER: '/home/amila/Desktop/meServer/temp/',
+    ME_SERVER: '/home/onbitlabs/meServer/temp/',
 
     server: {
-        host : 'http://localhost',
+        host : 'http://192.168.8.203',
         port : 1337
     },
 
-    ME_PORT :8080
+    ME_PORT :8081
 };
