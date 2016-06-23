1d97bfcf8d5ba47b1c3cc9972cdd7ee778c4e75c
shamila <shamila@onbitlabs.com>
2016-06-23 09:53:14 +0530

    Adding enter key press event to the register page

--- assets/js/controllers/auth/loginController.js
+++ assets/js/controllers/auth/loginController.js
@@ -8,18 +8,6 @@
       });
     }
   });
-   angular.module('app').directive('ngEnter', function() {
-      return function (scope, element, attrs) {
-                  element.bind("keydown keypress", function (event) {
-                      if(event.which === 13) {
-                          scope.$apply(function (){
-                              scope.$eval(attrs.ngEnter);
-                          });
-                          event.preventDefault();
-                      }
-                  });
-              };
-    });
 
   angular.module('app')
       .controller('LoginController',['$scope','$state','Auth','toastr','$auth', LoginController]);
