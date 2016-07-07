33d4f357ad99c1873ccccd24fc9e1da1c80b3bbb
Amila Sampath <amilaonbit@gmail.com>
2016-07-07 11:40:52 +0530

    ME637 : TemplateCategory.js dat move to TemplateCategoryDetails.js

---
+++ api/models/TemplateCategoryDetails.js
@@ -0,0 +1,26 @@
+/**
+ * TemplateCategoryDetails.js
+ *
+ * @description :: Here put, template category details : template category Id, template category name
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ *
+ * Created by amila on 7/07/16.
+ */
+
+module.exports = {
+
+  PortFolio : {
+    ID : '1',
+    Name : 'PortFolio'
+  },
+  Business : {
+    ID : '2',
+    Name : 'Business'
+  },
+  Media : {
+    ID : '3',
+    Name : 'Media'
+  }
+
+};
+
