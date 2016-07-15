5f7978563e37e920008959ee77510a5ecc0d9650
Madhuranga <madhuranga@onbitlabs.com>
2016-07-15 16:07:52 +0530

    Variant section has been changed

--- assets/js/services/edit/commerce/commerceService.js
+++ assets/js/services/edit/commerce/commerceService.js
@@ -1,202 +1,202 @@
 /**
  * Created by udeshikaperera on 26/08/2015.
  */
-(function() {
+(function () {
     angular.module('appEdit').service('commerceService', [
-        '$mdDialog', '$http','$rootScope','Upload', 'SERVER_URL','toastr', commerceService
+        '$mdDialog', '$http', '$rootScope', 'Upload', 'SERVER_URL', 'toastr', commerceService
     ]);
 
-    function commerceService($mdDialog, $http,$rootScope,Upload,SERVER_URL,toastr) {
+    function commerceService($mdDialog, $http, $rootScope, Upload, SERVER_URL, toastr) {
         return {
-             showAddProductsDialog: function(item) {
-                            return $mdDialog.show({
-                                controller: 'ProductCtrl',
-                                templateUrl: 'user/edit/commerce/addPro.html',
-                                clickOutsideToClose: true,
-                                locals : {
-                                            item : item,
-                                          }
-                            }).then(function(answer) {
+            showAddProductsDialog: function (item) {
+                return $mdDialog.show({
+                    controller: 'ProductCtrl',
+                    templateUrl: 'user/edit/commerce/addPro.html',
+                    clickOutsideToClose: true,
+                    locals: {
+                        item: item,
+                    }
+                }).then(function (answer) {
 
-                                //$scope.status = 'You said the information was "' + answer + '".';
-                            });
-                        },
-            updateProductImage: function(file,imageUrl,proId,appId){
+                    //$scope.status = 'You said the information was "' + answer + '".';
+                });
+            },
+            updateProductImage: function (file, imageUrl, proId, appId) {
                 return Upload.upload({
                     url: SERVER_URL + 'edit/updateThirdNaviImage',
                     fields: {
                         'imageUrl': imageUrl,
-                        'categoryId' : proId,
-                        'appId' : appId
+                        'categoryId': proId,
+                        'appId': appId
 
                     },
                     file: file
                 });
             },
-            updateCategoryImage: function(file,imageUrl,catId,appId){
+            updateCategoryImage: function (file, imageUrl, catId, appId) {
                 return Upload.upload({
                     url: SERVER_URL + 'edit/updateSecondNaviImage',
                     fields: {
                         'imageUrl': imageUrl,
-                        'categoryId' : catId,
-                        'appId' : appId
+                        'categoryId': catId,
+                        'appId': appId
                     },
                     file: file
                 });
             },
 
-            addProduct: function(file,product,id,variants){
+            addProduct: function (file, product, id, variants) {
                 return Upload.upload({
                     url: SERVER_URL + 'edit/addThirdNavigation',
                     fields: {
-                        id:id,
-                        appId:$rootScope.appId,
-                        mainId:product.mainId,
+                        id: id,
+                        appId: $rootScope.appId,
+                        mainId: product.mainId,
                         detailedDesc: product.detailedDesc,
                         name: product.name,
                         childId: variants.childId,
                         desc: product.desc,
-                        sku: product.sku,
+                        sku: variants.sku,
                         price: variants.price,
                         quantity: variants.quantity,
-                        type : product.type,
+                        type: product.type,
                         discount: product.discount
                     },
                     file: file
                 });
             },
-            showOrderDialog: function() {
+            showOrderDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/manageOrderView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
                 });
             },
-            showInventoryDialog: function() {
+            showInventoryDialog: function () {
                 return $mdDialog.show({
                     controller: 'InventoryCtrl',
                     templateUrl: 'user/edit/commerce/manageInventoryView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
                 });
             },
 
-            showTaxesDialog: function() {
+            showTaxesDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/manageTaxesView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     //$scope.status = 'You cancelled the dialog.';
                 });
             },
-            showAddTaxOptionDialog: function() {
+            showAddTaxOptionDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/addTaxOptionView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     //$scope.status = 'You cancelled the dialog.';
                 });
             },
-            showEmailSettingsDialog: function() {
+            showEmailSettingsDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/manageEmailSettingsView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     //$scope.status = 'You cancelled the dialog.';
                 });
             },
-            showStoreSettingsDialog: function() {
+            showStoreSettingsDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/manageStoreSettingsView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     //$scope.status = 'You cancelled the dialog.';
                 });
             },
-            showAddNewUserDialog: function() {
+            showAddNewUserDialog: function () {
                 return $mdDialog.show({
                     controller: 'CommerceCtrl',
                     templateUrl: 'user/edit/commerce/addNewUserView.html',
                     clickOutsideToClose: true
-                }).then(function(answer) {
+                }).then(function (answer) {
                     //$scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     //$scope.status = 'You cancelled the dialog.';
                 });
             },
-            showPolicies:function(appId){
-                return $http.get(SERVER_URL+ 'edit/showPolicies?appId='+appId);
+            showPolicies: function (appId) {
+                return $http.get(SERVER_URL + 'edit/showPolicies?appId=' + appId);
             },
-            savePolicies:function(appParams){
-                return $http.post(SERVER_URL+ 'edit/savePolicies',appParams);
+            savePolicies: function (appParams) {
+                return $http.post(SERVER_URL + 'edit/savePolicies', appParams);
             },
-            showStoreSettings:function(appId){
-                return $http.get(SERVER_URL+ 'edit/showStoreSettings?appId='+appId);
+            showStoreSettings: function (appId) {
+                return $http.get(SERVER_URL + 'edit/showStoreSettings?appId=' + appId);
             },
-            saveStoreSettings: function(appParams){
-                return $http.post(SERVER_URL+ 'edit/saveStoreSettings',appParams);
+            saveStoreSettings: function (appParams) {
+                return $http.post(SERVER_URL + 'edit/saveStoreSettings', appParams);
             },
-            getChild:function(childId){
-             return $http.get(SERVER_URL+ 'edit/getChild?childId='+childId);
+            getChild: function (childId) {
+                return $http.get(SERVER_URL + 'edit/getChild?childId=' + childId);
             },
-            getUpdates:function(id){
-                   return $http.get(SERVER_URL+ 'edit/getUpdates?ObjectId='+id);
+            getUpdates: function (id) {
+                return $http.get(SERVER_URL + 'edit/getUpdates?ObjectId=' + id);
             },
 //            getVariants:function(idDetails){
 //                return $http.get(SERVER_URL+ 'edit/getVariants?appId='+$rootScope.appId+'&childId='+idDetails);
 //            },
-            addPriceandVariants:function(variants,file){
-                if (file!== null){
+            addPriceandVariants: function (variants, file) {
+                if (file !== null) {
                     return Upload.upload({
                         url: SERVER_URL + 'edit/addVariants',
-                        fields : variants,
-                        file:file
+                        fields: variants,
+                        file: file
                     });
-                }else {
-                    return $http.post(SERVER_URL+ 'edit/addVariants',variants);
+                } else {
+                    return $http.post(SERVER_URL + 'edit/addVariants', variants);
                 }
 
             },
-            getMainMenuList: function(){
-                return $http.get(SERVER_URL+ 'edit/getMainNavigation?appId='+$rootScope.appId);
+            getMainMenuList: function () {
+                return $http.get(SERVER_URL + 'edit/getMainNavigation?appId=' + $rootScope.appId);
             },
-            getCategoryList: function(){
-                return $http.get(SERVER_URL+ 'edit/getSecondNavigation?appId='+$rootScope.appId);
+            getCategoryList: function () {
+                return $http.get(SERVER_URL + 'edit/getSecondNavigation?appId=' + $rootScope.appId);
             },
-            getProductList: function(){
-                return $http.get(SERVER_URL+ 'edit/getThirdNavigation?appId='+$rootScope.appId);
+            getProductList: function () {
+                return $http.get(SERVER_URL + 'edit/getThirdNavigation?appId=' + $rootScope.appId);
             },
-            getOrderList: function(){
-                return $http.get(SERVER_URL+ 'edit/getOrders?appId='+$rootScope.appId);
-            },
-
-            updateProductList : function(data){
-                return $http.post(SERVER_URL+ 'edit/updateThirdNavi',data);
-            },
-            checkCategory : function(data){
-                return $http.post(SERVER_URL+ 'edit/checkSecondNavi',data);
-            },
-            deleteCategoryData : function(data){
-                return $http.post(SERVER_URL+ 'edit/deleteSecondNavi',data);
+            getOrderList: function () {
+                return $http.get(SERVER_URL + 'edit/getOrders?appId=' + $rootScope.appId);
             },
 
-            updateInventory : function(data){
-                return $http.post(SERVER_URL+ 'edit/updateInventory',data);
+            updateProductList: function (data) {
+                return $http.post(SERVER_URL + 'edit/updateThirdNavi', data);
+            },
+            checkCategory: function (data) {
+                return $http.post(SERVER_URL + 'edit/checkSecondNavi', data);
+            },
+            deleteCategoryData: function (data) {
+                return $http.post(SERVER_URL + 'edit/deleteSecondNavi', data);
+            },
+
+            updateInventory: function (data) {
+                return $http.post(SERVER_URL + 'edit/updateInventory', data);
             },
 
             saveEmailDeliInfo: function (data) {
@@ -208,23 +208,23 @@
             sendTestEmail: function (data) {
                 return $http.post(SERVER_URL + 'edit/sendTestEmail', data);
             },
-            getEmailSettings : function(data){
-                return $http.post(SERVER_URL+ 'edit/getEmailSettings',data);
+            getEmailSettings: function (data) {
+                return $http.post(SERVER_URL + 'edit/getEmailSettings', data);
             },
-            getAllSiteType: function(){
+            getAllSiteType: function () {
                 return $http.get(SERVER_URL + 'edit/getAllSiteType');
             },
-            getAllMeasurementType: function(){
+            getAllMeasurementType: function () {
                 return $http.get(SERVER_URL + 'edit/getAllMeasurementType');
             },
-            getAllTimeAndRegion: function(){
+            getAllTimeAndRegion: function () {
                 return $http.get(SERVER_URL + 'edit/getAllTimeAndRegion');
             },
-            deleteProducts: function(data){
-                return $http.post(SERVER_URL+ 'edit/deleteThirdNavigation',data);
+            deleteProducts: function (data) {
+                return $http.post(SERVER_URL + 'edit/deleteThirdNavigation', data);
             },
-            updateOrders: function(data){
-                return $http.post(SERVER_URL+ 'edit/updateOrders',data)
+            updateOrders: function (data) {
+                return $http.post(SERVER_URL + 'edit/updateOrders', data)
             }
 
         };
