8971246223c566e0acdbd39d8a01e765ee4f1569
udeshika <udeshika@onbitlabs.com>
2016-07-19 09:36:42 +0530

    Merge branch 'variant_fix'
    # Conflicts:
    #	assets/js/services/edit/commerce/commerceService.js

--- config/connections.js
+++ config/connections.js
@@ -71,7 +71,7 @@ module.exports.connections = {
     port: 27017,
     // user: 'username',
     // password: 'password',
-    database: 'appBuilder'
+    database: 'otenro'
   },
 
   /***************************************************************************
