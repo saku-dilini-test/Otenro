be8829a44277dd666bffb3f1aa0a0f9139e50b9d
shamila <shamila@onbitlabs.com>
2016-07-21 17:35:11 +0530

    Changing the first page according to register and login

--- assets/js/controllers/auth/registerController.js
+++ assets/js/controllers/auth/registerController.js
@@ -8,7 +8,7 @@
         $scope.authSignUp = function (user) {
             Auth.register(user).success(function () {
                 toastr.success('Successfully registered ', 'Congratulations ! ', {closeButton: true});
-                $state.go('user.dashboard');
+                $state.go('anon.welcome');
 
             }).catch(function onError() {
                 toastr.error('Email already exists ' + $scope.user.email, 'Error', {
