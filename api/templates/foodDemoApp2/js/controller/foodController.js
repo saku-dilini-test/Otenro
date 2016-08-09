0e33607a829bf7bccfc7289e594d8e8584fca5bf
shamila <shamila@onbitlabs.com>
2016-08-09 17:59:43 +0530

    Slide show of images

--- api/templates/foodDemoApp2/js/controller/foodController.js
+++ api/templates/foodDemoApp2/js/controller/foodController.js
@@ -28,11 +28,17 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
         alert('warning', "Unable to get Products Selected Category", err.message);
     });
 
-
+    $scope.sliderOptions = {
+          initialSlide: 0,
+          direction: 'horizontal', //or vertical
+          speed: 300 //0.3s transition
+        };
+    $scope.sliderDelegate = null;
     if($stateParams.foodId){
         $http.get(constants.SERVER_URL + '/templates/getProductById?productId='+$stateParams.foodId)
              .success(function(data) {
                 $scope.foodInfo = data;
+                $scope.images = $scope.foodInfo.tempImageArray;
                 $scope.productVariants = data.variants;
 
                 if(data.variants.length > 0){
@@ -48,6 +54,7 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
                  alert('warning', "Unable to get Product", err.message);
           });
     }
+
 
     $scope.menuName = $stateParams.categoryName;
 
