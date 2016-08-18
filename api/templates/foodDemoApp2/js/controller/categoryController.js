02d625bfdfbbd794d2eaf8415d48de9ead8f2b99
prasanna <prasannabkmail@gmail.com>
2016-08-18 18:12:59 +0530

    app menu refresh issues

--- api/templates/foodDemoApp2/js/controller/categoryController.js
+++ api/templates/foodDemoApp2/js/controller/categoryController.js
@@ -26,13 +26,15 @@ mobileApp.controller('categoryCtrl', function($scope,$stateParams,$rootScope,$ht
         });
     }
 
+    $scope.userId=$rootScope.userId;
+    $scope.appId=$rootScope.appId;
 
     $scope.imageURL = constants.SERVER_URL
         +"/templates/viewImages?userId="
-        +$rootScope.userId+"&appId="+$rootScope.appId+"&"+new Date().getTime()+"&img=secondNavi";
+        +$scope.userId+"&appId="+$scope.appId+"&"+new Date().getTime()+"&img=secondNavi";
 
 
-     $http.get(constants.SERVER_URL + '/templates/getSpecificChild?appId='+$rootScope.appId).success(function(data) {
+     $http.get(constants.SERVER_URL + '/templates/getSpecificChild?appId='+$scope.appId).success(function(data) {
          $scope.categories = data;
      }).error(function(err) {
          alert('warning', "Unable to get categories", err.message);
