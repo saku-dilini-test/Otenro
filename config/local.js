b925460b24dc6480835e3ca38bf92ff228f9f2fa
udeshika <udeshika@onbitlabs.com>
2016-09-09 11:03:30 +0530

    Merge branch 'ME1362' into testServer1
    # Conflicts:
    #	api/controllers/edit/commerce/ThirdNavigationController.js

--- config/local.js
+++ config/local.js
@@ -80,14 +80,5 @@ module.exports = {
    * 'development' environment.                                              *
    ***************************************************************************/
   
-    //environment: process.env.NODE_ENV || 'development',
-
-    // HOST URL
-    HOST_URL : 'http://192.168.8.104',
-    // ME SERVER Config
-    ME_SERVER : '/home/amila/Desktop/meServer/temp/',
-    //ME_SERVER_PORT : 8080
-    // APP FILE SERVER Config
-    APP_FILE_SERVER : '/home/amila/Desktop/appFileServer/'
-
+    environment: process.env.NODE_ENV || 'development',
 };
