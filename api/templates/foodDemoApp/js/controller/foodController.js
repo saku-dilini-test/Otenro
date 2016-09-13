bd39a2cb0e844e5f81eccdfde78512e2b87842f0
Amila Sampath <amilaonbit@gmail.com>
2016-09-13 14:14:36 +0530

    ME-1443 , ME-1445 and ME-1446 FoodDemoApp added changes

--- api/templates/foodDemoApp/js/controller/foodController.js
+++ api/templates/foodDemoApp/js/controller/foodController.js
@@ -13,7 +13,8 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
     $scope.imageURL = constants.SERVER_URL
                 +"/templates/viewImages?userId="
                 +$scope.userId+"&appId="+$scope.appId+"&"+new Date().getTime()+"&img=thirdNavi";
-    
+    $scope.selectedVariant = {};
+
     $http.get(constants.SERVER_URL + '/templates/getProductsByCatId?appId='+$scope.appId+'&childId='+$stateParams.categoryId).success(function(data) {
         console.log("------- Start -----------");
         console.log(data);
@@ -62,6 +63,26 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
             $scope.isBuyBtnDisable = false;
         }else{
             $scope.isBuyBtnDisable = true;
+        }
+    };
+
+    // Check buyQty input value.
+    // If buyQty value is less than or equal Selected-Variant-Qty, Buy Button Enable
+    $scope.changeBuyQuantity = function (buyQty) {
+
+        // default : Buy button set as Disable
+        $scope.isBuyBtnDisable = true;
+
+        // if buyQty value greater than 0
+        if(buyQty > 0){
+            // Get Selected-Variant-Qty value
+            var selectVariantAvailableQty = $scope.selectedVariant.quantity;
+            if(typeof selectVariantAvailableQty != 'undefined'){
+                // If buyQty less than or equal Selected-Variant-Qty, buy button enable
+                if(buyQty <= selectVariantAvailableQty){
+                    $scope.isBuyBtnDisable = false;
+                }
+            }
         }
     };
 
