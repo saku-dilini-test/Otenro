caba103fdbc15bf550675d43ed0ac16fa1636716
Amila Sampath <amilaonbit@gmail.com>
2016-09-19 12:00:50 +0530

    TECC Mobile payment pending email send function commit to git

--- apps/cakeFactoryWebSite/js/controllers/mobileOrderCtrl.js
+++ apps/cakeFactoryWebSite/js/controllers/mobileOrderCtrl.js
@@ -30,6 +30,39 @@
             }
             $scope.conform = function(){
                 $scope.cart.checkout('PayPal','Mobile');
+            };
+
+            // before redirect to PayPal, Cart info send to server to save
+            $scope.saveCartInServer = function () {
+
+                $scope.deliveryOption = "pickUp";
+                $scope.isSubmitButtonDisableValue = 'true';
+                $scope.cart = DataService.cart;
+                var data = $scope.cart.getShoppingCart();
+                var shoppingCart = {
+                    'oneDoller' : $scope.cart.getOneDoller()
+                };
+                for(var i=0; i < data.length; i++){
+                    shoppingCart[i] = data[i];
+                }
+
+                shoppingCart['cartLength']	 = data.length;
+                shoppingCart['pickUpBranch'] = $scope.cart.getBranchName();
+                shoppingCart['deliveryCharge'] = $scope.cart.getDeliveryCharges();
+                shoppingCart['deliveryLocation'] = $scope.cart.getLocationName();
+                shoppingCart['deliveryAddress_01'] = $scope.cart.getDeliveryAddress_01();
+                shoppingCart['deliveryAddress_02'] = $scope.cart.getDeliveryAddress_02();
+                shoppingCart['name'] = $scope.cart.getName();
+                shoppingCart['city'] = $scope.cart.getCity();
+                shoppingCart['telPhone'] = $scope.cart.getTelPhone();
+                shoppingCart['comment'] = $scope.cart.getComment();
+                shoppingCart['pickUpDate'] = $scope.cart.getPickUpDate();
+                shoppingCart['pickUpTime'] = $scope.cart.getPickUpTime();
+                shoppingCart['paymentStatus'] = 'Pending';
+                //cart info save api
+                $http.post(SERVER_URL+"payment/saveShoppingCartWeb",shoppingCart)
+                    .then(function (response) {
+                    });
             }
 
         });
