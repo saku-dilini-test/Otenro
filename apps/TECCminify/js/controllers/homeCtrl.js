8a58d38289644dc36002fe18c2274271498c8ed9
KalaniParapitiya <kalani@onbitlabs.com>
2016-10-25 16:35:06 +0530

    Added Cart image.

--- apps/TECCminify/js/controllers/homeCtrl.js
+++ apps/TECCminify/js/controllers/homeCtrl.js
@@ -1,19 +1,22 @@
 
 (function () {
     "use strict";
-angular.module('animateApp')
-    .controller('homeCtrl',['$scope','$http','SERVER_URL',function($scope, $http,SERVER_URL) {
-        $scope.SERVER_URL = SERVER_URL;
+    angular.module('animateApp')
+        .controller('homeCtrl', function($scope, $http,SERVER_URL) {
+            $scope.SERVER_URL = SERVER_URL;
 
-        $http.get(SERVER_URL+"categories/getCategories")
-            .then(function (response) {            
-                $scope.allCategories = response.data.result;            
-            });
-    }])
-    .controller('navigationController',['$scope','$location',function($scope, $location) {
-        //Set selected state of navigation
-        $scope.isActive = function(route) {
-            return route === $location.path();
-        }
-    }]);
+            $http.get(SERVER_URL+"categories/getCategories")
+                .then(function (response) {
+                    $scope.allCategories = response.data.result;
+                });
+        })
+        .controller('navigationController', function($scope, $location, DataService) {
+            //Set selected state of navigation
+            $scope.isActive = function(route) {
+                return route === $location.path();
+            }
+            $scope.cart = DataService.cart;
+        });
 })();
+
+
