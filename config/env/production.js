3ff42a0209de5766243725a75a5ecbe6d0ec4ef0
udeshika <udeshika@onbitlabs.com>
2016-11-18 13:34:19 +0530

    Production configuration

--- config/env/production.js
+++ config/env/production.js
@@ -28,9 +28,9 @@ module.exports = {
   port: 443,
 
   ssl: {
-    ca: require('fs').readFileSync(require('path').resolve(__dirname,'../ssl/ca_bundle.crt')),
-    key: require('fs').readFileSync(require('path').resolve(__dirname,'../ssl/private.key')),
-    cert: require('fs').readFileSync(require('path').resolve(__dirname,'../ssl/certificate.crt'))
+    ca: require('fs').readFileSync(__dirname + '/ssl/ca_bundle.crt'),
+    key: require('fs').readFileSync(__dirname + '/ssl/private.key'),
+    cert: require('fs').readFileSync(__dirname + '/ssl/certificate.crt')
   },
 
   /***************************************************************************
