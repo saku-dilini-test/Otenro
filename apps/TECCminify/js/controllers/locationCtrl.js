5da754e4b084a1af7a432d20bca7d888e7004bab
KalaniParapitiya <kalani@onbitlabs.com>
2017-02-07 16:51:56 +0530

    cake company safari browser issues fixed.

--- apps/TECCminify/js/controllers/locationCtrl.js
+++ apps/TECCminify/js/controllers/locationCtrl.js
@@ -1,59 +1,59 @@
 
 (function () {
-	"use strict";
+    "use strict";
 
-	angular.module('animateApp')
-		.controller('locationCtrl',['$scope','$http','SERVER_URL','DataService','$location', function($scope, $http,SERVER_URL,DataService,$location) {
-			$scope.deliveryOption = "pickUp"
-			$scope.cart = DataService.cart;
+angular.module('animateApp')
+	.controller('locationCtrl', function($scope, $http,SERVER_URL,DataService,$location) {
+		$scope.deliveryOption = "pickUp";
+		$scope.cart = DataService.cart;
+		$scope.isPassedDate = false;
+		$scope.isOutOfHours = false;
+
+		$http.get(SERVER_URL+"locations/getBranchLocations")
+			.then(function (response) {
+	            $scope.branchLocations = response.data.result;
+          	});
+
+		$http.get(SERVER_URL+"locations/getDeliveryLocations")
+			.then(function (response) {
+	            $scope.deliveryLocations = response.data.result;
+          	});
+
+		$scope.selectDeliveryOption = function(option){
+			$scope.cart.selectDeliveryOption(option);
+			$scope.setPickUpDate = '';
+			$scope.setPickUpTime = '';
+			$scope.setDeliveryDate = '';
+			$scope.setDeliveryTime = '';
 			$scope.isPassedDate = false;
 			$scope.isOutOfHours = false;
+			$scope.comment = '';
+		}
 
-			$http.get(SERVER_URL+"locations/getBranchLocations")
-				.then(function (response) {
-					$scope.branchLocations = response.data.result;
-				});
+		$scope.saveBranchName = function(name){
+			$scope.cart.saveBranchName(name);
+			$scope.selectedBranch = $scope.cart.getBranchName();
+		}
 
-			$http.get(SERVER_URL+"locations/getDeliveryLocations")
-				.then(function (response) {
-					$scope.deliveryLocations = response.data.result;
-				});
+		$scope.saveLocationName = function(location){
+			if(!location){
+				$scope.cart.saveLocationName('');
+				$scope.cart.saveDeliveryCharges(0);
 
-			$scope.selectDeliveryOption = function(option){
-				$scope.cart.selectDeliveryOption(option);
-				$scope.setPickUpDate = '';
-				$scope.setPickUpTime = '';
-				$scope.setDeliveryDate = '';
-				$scope.setDeliveryTime = '';
-				$scope.isPassedDate = false;
-				$scope.isOutOfHours = false;
-
+			}else{
+				$scope.cart.saveLocationName(location.locationName);
+				$scope.cart.saveDeliveryCharges(location.deliveryCharge);
 			}
+		}
 
-			$scope.saveBranchName = function(name){
-				$scope.cart.saveBranchName(name);
-				$scope.selectedBranch = $scope.cart.getBranchName();
-			}
-
-			$scope.saveLocationName = function(location){
-				if(!location){
-					$scope.cart.saveLocationName('');
-					$scope.cart.saveDeliveryCharges(0);
-
-				}else{
-					$scope.cart.saveLocationName(location.locationName);
-					$scope.cart.saveDeliveryCharges(location.deliveryCharge);
-				}
-			}
-
-			$scope.saveDeliveryAddress = function(address){
-				$scope.cart.saveDeliveryAddress_01(address.line01);
-				$scope.cart.saveDeliveryAddress_02(address.line02);
-				$scope.cart.saveName(address.name);
-				$scope.cart.saveCity(address.city);
-				$scope.cart.saveTelPhone(address.contactNumber);
-				$scope.cart.saveEmail(address.email);
-			}
+		$scope.saveDeliveryAddress = function(address){
+			$scope.cart.saveDeliveryAddress_01(address.line01);
+			$scope.cart.saveDeliveryAddress_02(address.line02);
+			$scope.cart.saveName(address.name);
+			$scope.cart.saveCity(address.city);
+			$scope.cart.saveTelPhone(address.contactNumber);
+			$scope.cart.saveEmail(address.email);
+		}
 
 		$scope.savePickUpDate = function(date){
 			var pickDate = date.getDate() + ' - ' + ( date.getMonth() + 1 ) + ' - ' + date.getFullYear();
@@ -80,7 +80,9 @@
 					$scope.isOutOfHours = true;
 				}
 			}
-		}
+		};
+
+
 		// save Delivery Date and Time
 		$scope.saveDeliveryDate = function(date){
 			var deliveryDate = date.getDate() + ' - ' + ( date.getMonth() + 1 ) + ' - ' + date.getFullYear();
@@ -92,6 +94,7 @@
 			$scope.cart.saveDeliveryTime(deliveryTime);
 
 			var currentDate = new Date();
+
 			$scope.isOutOfHours = false;
 			if(date < currentDate){
 				$scope.isPassedDate = true;
@@ -127,10 +130,10 @@
 			$scope.comment = comment;
 			$scope.cart.saveComment(comment);
 		}
-			$scope.goShoppingCart = function () {
-				$location.path('/shoppingCart');
-			};
+		$scope.goShoppingCart = function () {
+			$location.path('/shoppingCart');
+		};
 
 
-		}]);
+	});
 })();
