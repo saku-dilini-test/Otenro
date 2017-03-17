10ef5524a1bd6bdb6d3c78e97c1f1e83f4f48e56
prasanna <prasannabkmail@gmail.com>
2017-03-17 10:30:24 +0530

    ME-2235 fixed paypal payment bugs

--- source/api/controllers/edit/commerce/IPGController.js
+++ source/api/controllers/edit/commerce/IPGController.js
@@ -4,7 +4,11 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var fs = require('fs-extra'),
+    xml2js = require('xml2js'),
+    config = require('../../../services/config');
 module.exports = {
+
 
     /**
      * update IPG Details collections for given IPG Details Id
@@ -13,18 +17,62 @@ module.exports = {
      * @param res
      */
     updateIPGInfo : function(req,res){
+
         var searchQuery = {
             id : req.body.id
         };
+
+        var payPalEnv = '';
+        var authKey = '';
+        if (sails.config.environment === 'production'){
+            payPalEnv = "PayPalEnvironmentProduction";
+            authKey = 'prod-xxx';
+        }else {
+            payPalEnv = "PayPalEnvironmentSandbox";
+            authKey = 'sandbox-xxx';
+        }
+        var tempAppDirPath = config.ME_SERVER + req.body.userId + '/templates/' + req.body.appId;
         var updateData = req.body;
         if(typeof req.body.id != 'undefined'){
-            IPGDetails.update(searchQuery,updateData,function(err,main) {
+            IPGDetails.update(searchQuery,updateData,function(err,ipg) {
                 if (err) return res.send(err);
+                console.log(JSON.stringify(ipg[0].paypalKey));
+                if (typeof ipg[0].paypalKey !='undefined'){
+                    var appJsFilePath = tempAppDirPath + '/js/app.js';
+                    fs.readFile(appJsFilePath, 'utf-8',
+                        function (err, data) {
+                            if (err) return res.negotiate(err);
+
+                                              
+                             data = data.replace(authKey, ipg[0].paypalKey);
+                             data = data.replace("paypalEnv",payPalEnv);
+
+                            fs.writeFile(appJsFilePath, data , 'utf-8', function (err) {
+                                if (err) return res.negotiate(err);
+                            });
+                        });
+                }
                 return res.send(200, {message: 'IPG Details successfully updated'});
             });
         }else{
-            IPGDetails.create(updateData).exec(function (err, result) {
+            IPGDetails.create(updateData).exec(function (err, ipg) {
                 if (err) return res.send(err);
+                console.log(JSON.stringify(ipg));
+                if (typeof ipg.paypalKey !='undefined'){
+                    var appJsFilePath = tempAppDirPath + '/js/app.js';
+                    fs.readFile(appJsFilePath, 'utf-8',
+                        function (err, data) {
+                            if (err) return res.negotiate(err);
+
+                            
+                            data = data.replace(authKey, ipg.paypalKey);
+                            data = data.replace("paypalEnv",payPalEnv);
+
+                            fs.writeFile(appJsFilePath, data , 'utf-8', function (err) {
+                                if (err) return res.negotiate(err);
+                            });
+                        });
+                }
                 return res.send(200, {message: 'IPG Details successfully created'});
             });
         }
