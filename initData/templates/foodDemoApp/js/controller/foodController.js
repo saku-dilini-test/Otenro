0beba8ae09d075d60e1021a0f75d2071b52feeab
OnbitShashan <shashan@onbitlabs.com>
2017-04-24 13:25:53 +0530

    ME-2346 fixed quantity not changing to 0 in apps

--- initData/templates/foodDemoApp/js/controller/foodController.js
+++ initData/templates/foodDemoApp/js/controller/foodController.js
@@ -73,6 +73,8 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
       $scope.selection1 =[];
       $scope.buyQuantity = {};
       $scope.selectedVariant1  =variant.vType;
+      $scope.selectedVariant.buyQuantity = '';
+
       $scope.lockBuyButton = true;
 
         if($scope.foodInfo.selection.length==1){
@@ -92,9 +94,13 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
 
     };
 
-    $scope.changeVariant2 = function(variant) {
+    $scope.changeVariant2 = function(variant){
       $scope.selection2 =[];
-      $scope.selectedVariant2  =variant.vType;
+      if(variant){
+            $scope.selectedVariant2  =variant.vType;
+            $scope.selectedVariant.buyQuantity = '';
+
+      }
       $scope.lockBuyButton = true;
 
         if($scope.foodInfo.selection.length==2){
@@ -118,7 +124,11 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
 
     $scope.changeVariant3 = function(variant){
       $scope.selection3 =[];
-      $scope.selectedVariant3  =variant.vType;
+      if(variant){
+            $scope.selectedVariant3  =variant.vType;
+            $scope.selectedVariant.buyQuantity = '';
+
+      }
       $scope.lockBuyButton = true;
 
         if($scope.foodInfo.selection.length==3){
@@ -140,7 +150,11 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
         }
     };
     $scope.changeVariant4 = function(variant){
-      $scope.selectedVariant4  =variant.vType;
+        if(variant){
+            $scope.selectedVariant4  =variant.vType;
+            $scope.selectedVariant.buyQuantity = '';
+
+        }
 
       for(var i=0;i<$scope.foodInfo.variants.length;i++){
             if($scope.foodInfo.variants[i].selection[0].vType == $scope.selectedVariant1 &&
@@ -152,7 +166,6 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
 
             }
       }
-      console.log($scope.selectedVariant)
     };
 
 
