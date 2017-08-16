58d630026c436ad05b6433909e3f4f384b8a1ff0
Praveen <praveen@onbitlabs.com>
2017-08-16 13:54:37 +0530

    ME-2377 Send email to user on inactivity

--- source/config/cron.js
+++ source/config/cron.js
@@ -4,8 +4,8 @@
 
 module.exports.cron = {
     myFirstJob: {
-        //schedule: '00 00 01 * * *',
-        schedule: '*/10 * * * * *',
+        schedule: '00 00 01 * * *',
+        //schedule: '*/10 * * * * *',
         onTick: function () {
             if(sails){
                 /*runs everyday at 1 am*/
