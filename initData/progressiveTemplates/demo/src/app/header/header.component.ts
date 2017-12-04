7f0a87ad835a48a1649c15e4b72a730b4eac4f43
cleanCocain <s.dilakshan@yahoo.com>
2017-12-04 16:00:10 +0530

    ME-2623 register and login form validation and logout function.

--- initData/progressiveTemplates/demo/src/app/header/header.component.ts
+++ initData/progressiveTemplates/demo/src/app/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PagebodyServiceModule } from '../page-body/page-body.service';
-
+import { LocalStorageService } from 'angular-2-local-storage';
+import * as data from './../madeEasy.json';
 
 @Component({
   selector: 'app-header',
@@ -9,21 +10,30 @@ import { PagebodyServiceModule } from '../page-body/page-body.service';
   styleUrls: ['./app/header/header.component.css']
 })
 export class HeaderComponent {
+  public appId = (<any>data).appId;
+  public userId = (<any>data).userId;
   cartNo:number;
   loginStatus;
-  constructor(private router: Router,private dataService : PagebodyServiceModule) {
+  constructor(private localStorageService: LocalStorageService,private router: Router,private dataService : PagebodyServiceModule) {
     this.cartNo = this.dataService.cart.cartItems.length;
   }
 
   ngAfterContentChecked()	  {
-    this.cartNo = this.dataService.cart.cartItems.length;
-    if(this.dataService.isUserLoggedIn.check == false){
-      this.loginStatus = "Not logged in" ;
-    }else{
-    this.loginStatus = "logged in ";
-    }
-  }
 
+    this.cartNo = this.dataService.cart.cartItems.length;
+
+    if(this.localStorageService.get('appLocalStorageUser' + this.appId) !== null ){
+      this.loginStatus = "logged in";
+    }else{
+      this.loginStatus = "Not logged in" ;
+    }
+
+  }
+  logout() {
+    this.localStorageService.remove('appLocalStorageUser' + this.appId);
+    this.dataService.isUserLoggedIn.check = false;
+    this.router.navigate(['home']);
+  }
   navigate(val:string){
     this.router.navigate([val])
   }
