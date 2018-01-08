522b580e79bdc65eeba30305149a0786b8f5c5f2
OnbitShashan <shashan@onbitlabs.com>
2018-01-08 15:23:54 +0530

    ME-2638 fixed headphone app background should be lighter colors

--- initData/previewData/meServerFiles/unknownUser/templates/586cd65e641694232165c1b1/js/controller/foodController.js
+++ initData/previewData/meServerFiles/unknownUser/templates/586cd65e641694232165c1b1/js/controller/foodController.js
@@ -5,7 +5,7 @@
 mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$state,$ionicPopup,constants,$log) {
 
     $scope.$emit('hideMenu',{});
-
+    $rootScope.parseWeight;
     $scope.buyQuantity=0;
     $rootScope.timestamp = new Date().getTime();
     $scope.userId=$rootScope.userId;
@@ -23,13 +23,21 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
             }
         }
 
-    $scope.getRandomColor = function () {
-            var letters = '0123456789ABCDEF';
-            var color = 'background-color:#';
-            for (var i = 0; i < 6; i++ ) {
-                color += letters[Math.floor(Math.random() * 16)];
+     var check ='background-color:#';
+     var color = 'background-color:#';
+     $scope.getRandomColor = function () {
+        var letters = ["CFBDAC", "D0DDDE", "EEEEEE", "FFDE8B", "DEBBAF", "C6D3E4"];
+
+        while(true){
+            console.log(color);
+            if(check !== color){
+                check = color;
+                return color;
+            }else {
+                color = 'background-color:#';
+                color += letters[Math.floor(Math.random()*letters.length)];
             }
-            return color;
+        }
     };
 
     $scope.color = [];
@@ -221,13 +229,18 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
                     var i=0;
                     while(i < $rootScope.cart.cartItems.length){
                         if($scope.foodInfo.id == $rootScope.cart.cartItems[i].id && $scope.selectedVariant.sku == $rootScope.cart.cartItems[i].sku){
+                            $rootScope.position2 = false;
+                            //increasing weight when we add same product again.
+                            $rootScope.cart.cartItems[i].totWeight += $scope.selectedVariant.weight*$scope.selectedVariant.buyQuantity;
                             $rootScope.cart.cartItems[i].qty += $scope.selectedVariant.buyQuantity;
                             $rootScope.cart.cartSize = $rootScope.cart.cartItems.length;
                             $scope.parentobj.cartSize = $rootScope.cart.cartSize;
+                            $rootScope.parseWeight = $scope.selectedVariant.weight;
                             $state.go('app.category');
                             break;
                         }
                         else if(i == ($rootScope.cart.cartItems.length -1)){
+                            $rootScope.position2 = true;
                             $rootScope.cart.cartItems.push({
                                 id: $scope.foodInfo.id,
                                 name: $scope.foodInfo.name,
@@ -237,9 +250,11 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
                                 price: $scope.selectedVariant.price,
                                 total : $scope.selectedVariant.price,
                                 imgURL : $stateParams.item.tempImageArray,
-                                totalQty: $scope.selectedVariant.quantity
+                                totalQty: $scope.selectedVariant.quantity,
+                                weight: $scope.selectedVariant.weight  //(new) added weight of each product
 
                             });
+                            // $rootScope.cart.cartItems[i].totWeight = $scope.selectedVariant.weight*$scope.selectedVariant.buyQuantity + $scope.insideWeight;
                             $rootScope.cart.cartSize = $rootScope.cart.cartItems.length;
                             $scope.parentobj.cartSize = $rootScope.cart.cartSize;
                             $state.go('app.category');
@@ -258,10 +273,13 @@ mobileApp.controller('foodCtrl', function($scope,$stateParams,$rootScope,$http,$
                     price: $scope.selectedVariant.price,
                     total : $scope.selectedVariant.price,
                     imgURL : $stateParams.item.tempImageArray,
-                    totalQty: $scope.selectedVariant.quantity
+                    totalQty: $scope.selectedVariant.quantity,
+                    weight: $scope.selectedVariant.weight //(new) added weight of each product
+
                 });
                 $rootScope.cart.cartSize = $rootScope.cart.cartItems.length;
                 $scope.parentobj.cartSize = $rootScope.cart.cartSize;
+                $rootScope.parseWeight = $scope.selectedVariant.weight;
                 $state.go('app.category');
             }
         }
