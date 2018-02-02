668279f4b633cc1d2f6c55ea8584845cdd44902b
prasannaonbitlabs <prasanna@onbitlabs.com>
2018-02-02 15:46:41 +0530

    ME-2729 Analytics - Time period - End date should not less than Start date

--- source/assets/js/controllers/edit/analytics/AnalyticsController.js
+++ source/assets/js/controllers/edit/analytics/AnalyticsController.js
@@ -58,147 +58,189 @@
 
         $scope.getSalesSummary  = function (data) {
 
-            var postData = { appId: $rootScope.appId,
-                             selectedProducts: data.selectedProducts, 
-                             fromDate: data.fromDate,
-                             toDate: data.toDate };            
+            if (data.fromDate > data.toDate){
 
-            commerceService.getSalesSummary(postData)
-                .success(function (result) {
-                    $scope.total=0;
-                    $scope.salesList = result.data;
-
-                    angular.forEach($scope.salesList, function(sales){
-                        $scope.total = $scope.total + sales.totalPrice ;
-                    })
-
-                }).error(function (error) {
-                toastr.error('Loading Error', 'Warning', {
+                toastr.error('Invalid Date Range', 'Warning', {
                     closeButton: true
                 });
-            });
+            }else {
 
-            commerceService.getChartData(postData)
-                .success(function(result){
-                    $scope.salesChartdata = result.data;
-                }).error(function(error){
-                console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                $scope.salesChartdata = emptyChart;
-            });
+                var postData = {
+                    appId: $rootScope.appId,
+                    selectedProducts: data.selectedProducts,
+                    fromDate: data.fromDate,
+                    toDate: data.toDate
+                };
 
+                commerceService.getSalesSummary(postData)
+                    .success(function (result) {
+                        $scope.total = 0;
+                        $scope.salesList = result.data;
+
+                        angular.forEach($scope.salesList, function (sales) {
+                            $scope.total = $scope.total + sales.totalPrice;
+                        })
+
+                    }).error(function (error) {
+                    toastr.error('Loading Error', 'Warning', {
+                        closeButton: true
+                    });
+                });
+
+                commerceService.getChartData(postData)
+                    .success(function (result) {
+                        $scope.salesChartdata = result.data;
+                    }).error(function (error) {
+                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                    $scope.salesChartdata = emptyChart;
+                });
+            }
         };
 
 
         $scope.getTaxSummary = function (data) {
 
-            var postData = { appId: $rootScope.appId, 
-                             fromDate: data.fromDate,
-                             toDate: data.toDate }            
+            if (data.fromDate > data.toDate){
 
-            commerceService.getTaxSummary(postData)
-                .success(function (result) {
-
-                    $scope.tax_total=0;
-                    $scope.taxList = result.data;
-
-                    angular.forEach($scope.taxList, function(tax){
-                        $scope.tax_total = $scope.tax_total + tax.taxTotal ;
-                    })
-
-                }).error(function (error) {
-                toastr.error('Loading Error', 'Warning', {
+                toastr.error('Invalid Date Range', 'Warning', {
                     closeButton: true
                 });
-            });
+            }else {
 
-            commerceService.getChartData(postData)
-                .success(function(result){
-                    $scope.taxChartdata = result.data;
-                }).error(function(error){
-                console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                $scope.taxChartdata = emptyChart;
-            });                
+                var postData = { appId: $rootScope.appId,
+                    fromDate: data.fromDate,
+                    toDate: data.toDate }
+
+                commerceService.getTaxSummary(postData)
+                    .success(function (result) {
+
+                        $scope.tax_total=0;
+                        $scope.taxList = result.data;
+
+                        angular.forEach($scope.taxList, function(tax){
+                            $scope.tax_total = $scope.tax_total + tax.taxTotal ;
+                        })
+
+                    }).error(function (error) {
+                    toastr.error('Loading Error', 'Warning', {
+                        closeButton: true
+                    });
+                });
+
+                commerceService.getChartData(postData)
+                    .success(function(result){
+                        $scope.taxChartdata = result.data;
+                    }).error(function(error){
+                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                    $scope.taxChartdata = emptyChart;
+                });
+
+            }
+
+
 
         };
 
         $scope.getShippingSummary = function (data) {
 
-            var postData = { appId: $rootScope.appId, 
-                             fromDate: data.fromDate,
-                             toDate: data.toDate }   
+            if (data.fromDate > data.toDate){
 
-            commerceService.getShippingSummary(postData)
-                .success(function (result) {
-
-                    $scope.shpping_total=0;
-                    $scope.shppingList = result.data;
-                    $scope.url ="/reports/getOrderData?appId="+$rootScope.appId+"&fromDate="+data.fromDate+"&toDate="+data.toDate;
-
-                    angular.forEach($scope.shppingList, function(tax){
-                        $scope.shpping_total = $scope.shpping_total + tax.totalShippingCost ;
-                    })
-
-                }).error(function (error) {
-                toastr.error('Loading Error', 'Warning', {
+                toastr.error('Invalid Date Range', 'Warning', {
                     closeButton: true
                 });
-            });
+            }else {
 
-            commerceService.getChartData(postData)
-                .success(function(result){
-                    $scope.shippingChartdata = result.data;
-                }).error(function(error){
-                console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                $scope.shippingChartdata = emptyChart;
-            });                
+                var postData = {
+                    appId: $rootScope.appId,
+                    fromDate: data.fromDate,
+                    toDate: data.toDate
+                }
 
+                commerceService.getShippingSummary(postData)
+                    .success(function (result) {
+
+                        $scope.shpping_total = 0;
+                        $scope.shppingList = result.data;
+                        $scope.url = "/reports/getOrderData?appId=" + $rootScope.appId + "&fromDate=" + data.fromDate + "&toDate=" + data.toDate;
+
+                        angular.forEach($scope.shppingList, function (tax) {
+                            $scope.shpping_total = $scope.shpping_total + tax.totalShippingCost;
+                        })
+
+                    }).error(function (error) {
+                    toastr.error('Loading Error', 'Warning', {
+                        closeButton: true
+                    });
+                });
+
+                commerceService.getChartData(postData)
+                    .success(function (result) {
+                        $scope.shippingChartdata = result.data;
+                    }).error(function (error) {
+                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                    $scope.shippingChartdata = emptyChart;
+                });
+            }
         };
 
         $scope.downloadFile = function (data,type) {
 
-             var paramData ;
+            if (!data.fromDate || !data.toDate) {
 
-             if (type=='sales'){
+                toastr.error('Please select the date range', 'Warning', {
+                    closeButton: true
+                });
+            } else {
 
-                 paramData = { appId: $rootScope.appId,
-                     fromDate:data.fromDate,toDate:data.toDate,selectedProducts:data.selectedProducts,type:type }
-             }else {
-                 paramData = { appId: $rootScope.appId,fromDate:data.fromDate,toDate:data.toDate,type:type };
-             }
+                var paramData;
 
-            $http({
-                method: 'POST',
-                url: '/reports/getOrderData',
-                data: paramData,
-                responseType: 'arraybuffer'
-            }).success(function (data, status, headers) {
-                headers = headers();
+                if (type == 'sales') {
 
-                var filename = headers['x-filename'];
-                var contentType = headers['content-type'];
-
-                var linkElement = document.createElement('a');
-                try {
-                    var blob = new Blob([data], { type: contentType });
-                    var url = window.URL.createObjectURL(blob);
-
-                    linkElement.setAttribute('href', url);
-                    linkElement.setAttribute("download", filename);
-
-                    var clickEvent = new MouseEvent("click", {
-                        "view": window,
-                        "bubbles": true,
-                        "cancelable": false
-                    });
-                    linkElement.dispatchEvent(clickEvent);
-                } catch (ex) {
-                    console.log(ex);
+                    paramData = {
+                        appId: $rootScope.appId,
+                        fromDate: data.fromDate,
+                        toDate: data.toDate,
+                        selectedProducts: data.selectedProducts,
+                        type: type
+                    }
+                } else {
+                    paramData = {appId: $rootScope.appId, fromDate: data.fromDate, toDate: data.toDate, type: type};
                 }
-            }).error(function (data) {
-                console.log(data);
-            });
-        };
 
+                $http({
+                    method: 'POST',
+                    url: '/reports/getOrderData',
+                    data: paramData,
+                    responseType: 'arraybuffer'
+                }).success(function (data, status, headers) {
+                    headers = headers();
+
+                    var filename = headers['x-filename'];
+                    var contentType = headers['content-type'];
+
+                    var linkElement = document.createElement('a');
+                    try {
+                        var blob = new Blob([data], {type: contentType});
+                        var url = window.URL.createObjectURL(blob);
+
+                        linkElement.setAttribute('href', url);
+                        linkElement.setAttribute("download", filename);
+
+                        var clickEvent = new MouseEvent("click", {
+                            "view": window,
+                            "bubbles": true,
+                            "cancelable": false
+                        });
+                        linkElement.dispatchEvent(clickEvent);
+                    } catch (ex) {
+                        console.log(ex);
+                    }
+                }).error(function (data) {
+                    console.log(data);
+                });
+            }
+            ;
+        }
     }
 
 
