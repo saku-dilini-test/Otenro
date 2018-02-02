be4b041f5c119237e3c2da24754391bdf3c7629d
OnbitShashan <shashan@onbitlabs.com>
2018-02-02 17:02:02 +0530

    ME-2723 ui for add banner carousel

--- source/assets/js/controllers/edit/carousel/carouselController.js
+++ source/assets/js/controllers/edit/carousel/carouselController.js
@@ -26,6 +26,56 @@
         $scope.cancel = function () {
             $mdDialog.cancel();
         };
+
+
+
+        // controller for add carousel banner
+
+        $scope.imageSelected = true;
+        $scope.buttonName = "Browse Image";
+
+
+        // image crop function
+        $scope.cropImage = function () {
+            var handleFileSelect=function(evt) {
+                var file=evt.currentTarget.files[0];
+                var reader = new FileReader();
+                reader.onload = function (evt) {
+                    $scope.$apply(function($scope){
+                        $scope.myImage=evt.target.result;
+                        $scope.picFile =  $scope.myImage
+                    });
+                };
+                reader.readAsDataURL(file);
+                $scope.imageSelected =false;
+                $scope.buttonName = "Upload";
+            };
+            angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
+        }
+
+        $scope.addImage = function(img){
+            if(angular.element('#fileInput').val() == ''){
+                toastr.error('Please choose an image to upload', 'Warning', {
+                    closeButton: true
+                });
+            }
+            else{
+                var im = $scope.tmpImage;
+                im[0] = $scope.picFile;
+                $scope.tmpImage = im;
+                $scope.mainImg = img;
+                $scope.myImage = null;
+                toastr.success('Image has been uploaded successfully', 'message', {
+                    closeButton: true
+                });
+            }
+
+            $scope.imageSelected = true;
+            $scope.buttonName = "Browse Image";
+        };
+
+
+
     };
 
 })();
