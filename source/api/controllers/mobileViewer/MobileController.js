eba92a8545c879d1ca63116f32646b7f467aa32e
OnbitShashan <shashan@onbitlabs.com>
2018-02-06 13:03:16 +0530

    changed ip of payhere

--- source/api/controllers/mobileViewer/MobileController.js
+++ source/api/controllers/mobileViewer/MobileController.js
@@ -53,8 +53,8 @@ module.exports = {
 
              //var currency = req.param("currency");
              var currency = "LKR";
-             //var sever = config.server.host+':'+config.server.port;
-             var  sever = "http://76c8163f.ngrok.io";
+             var sever = config.server.host+':'+config.server.port;
+             //var  sever = "http://76c8163f.ngrok.io";
 
         var htmlForm ="<!DOCTYPE html> <html lang=\"en\"> <head> <meta charset=\"utf-8\"> " +
                         "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> " +
