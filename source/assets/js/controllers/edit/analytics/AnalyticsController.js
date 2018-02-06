03d98345fb372d583696341686fe620e529c8405
prasannaonbitlabs <prasanna@onbitlabs.com>
2018-02-06 11:56:29 +0530

    ME-2739 Analytics Sales It should be disabled the apply button when only selecting a date range

--- source/assets/js/controllers/edit/analytics/AnalyticsController.js
+++ source/assets/js/controllers/edit/analytics/AnalyticsController.js
@@ -51,195 +51,305 @@
 
 
 
+
         $scope.searchTerm;
         $scope.clearSearchTerm = function() {
             $scope.searchTerm = '';
         };
 
         $scope.getSalesSummary  = function (data) {
+            var fromDate ;
+            var toDate ;
+            var selectedData;
+            $scope.total=null;
+            $scope.salesChartdata =null;
+            $scope.salesList=null;
+            $scope.salesChartdata=null;
 
-            if (data.fromDate > data.toDate){
-
-                toastr.error('Invalid Date Range', 'Warning', {
-                    closeButton: true
-                });
-            }else {
-
-                var postData = {
-                    appId: $rootScope.appId,
-                    selectedProducts: data.selectedProducts,
-                    fromDate: data.fromDate,
-                    toDate: data.toDate
-                };
-
-                commerceService.getSalesSummary(postData)
-                    .success(function (result) {
-                        $scope.total = 0;
-                        $scope.salesList = result.data;
-
-                        angular.forEach($scope.salesList, function (sales) {
-                            $scope.total = $scope.total + sales.totalPrice;
-                        })
-
-                    }).error(function (error) {
-                    toastr.error('Loading Error', 'Warning', {
+            try{
+                data.fromDate.toString().length;
+                data.toDate.toString().length;
+                fromDate = data.fromDate;
+                toDate = data.toDate;
+                selectedData = data.selectedProducts;
+                if(selectedData.length < 1){
+                    toastr.error('Please fill all fields', 'Warning', {
                         closeButton: true
                     });
+                }
+
+            }catch(e){
+                fromDate =null;
+                toDate=null;
+                toastr.error('Please fill all fields', 'Warning', {
+                    closeButton: true
                 });
 
-                commerceService.getChartData(postData)
-                    .success(function (result) {
-                        $scope.salesChartdata = result.data;
-                    }).error(function (error) {
-                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                    $scope.salesChartdata = emptyChart;
-                });
             }
+
+            if(fromDate&&toDate&&selectedData.length > 0){
+                if (fromDate >toDate){
+
+                    toastr.error('Invalid Date Range', 'Warning', {
+                        closeButton: true
+                    });
+                }else {
+
+                    var postData = {
+                        appId: $rootScope.appId,
+                        selectedProducts: data.selectedProducts,
+                        fromDate: data.fromDate,
+                        toDate: data.toDate
+                    };
+
+                    commerceService.getSalesSummary(postData)
+                        .success(function (result) {
+                            $scope.total = 0;
+                            $scope.salesList = result.data;
+
+                            angular.forEach($scope.salesList, function (sales) {
+                                $scope.total = $scope.total + sales.totalPrice;
+                            })
+
+                        }).error(function (error) {
+                        toastr.error('Loading Error', 'Warning', {
+                            closeButton: true
+                        });
+                    });
+
+                    commerceService.getChartData(postData)
+                        .success(function (result) {
+                            $scope.salesChartdata = result.data;
+                        }).error(function (error) {
+                        console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                        $scope.salesChartdata = emptyChart;
+                    });
+                }
+
+            }
+
         };
 
 
         $scope.getTaxSummary = function (data) {
+            var fromDate ;
+            var toDate ;
+            var selectedData;
+            $scope.tax_total=null;
+            $scope.salesChartdata =null;
+            $scope.taxList=null;
+            $scope.taxChartdata =null;
 
-            if (data.fromDate > data.toDate){
+            try{
 
-                toastr.error('Invalid Date Range', 'Warning', {
+                data.fromDate.toString().length;
+                data.toDate.toString().length;
+                fromDate = data.fromDate;
+                toDate = data.toDate;
+
+            }catch(e){
+                toastr.error('Please fill all fields', 'Warning', {
                     closeButton: true
-                });
-            }else {
-
-                var postData = { appId: $rootScope.appId,
-                    fromDate: data.fromDate,
-                    toDate: data.toDate }
-
-                commerceService.getTaxSummary(postData)
-                    .success(function (result) {
-
-                        $scope.tax_total=0;
-                        $scope.taxList = result.data;
-
-                        angular.forEach($scope.taxList, function(tax){
-                            $scope.tax_total = $scope.tax_total + tax.taxTotal ;
-                        })
-
-                    }).error(function (error) {
-                    toastr.error('Loading Error', 'Warning', {
-                        closeButton: true
-                    });
-                });
-
-                commerceService.getChartData(postData)
-                    .success(function(result){
-                        $scope.taxChartdata = result.data;
-                    }).error(function(error){
-                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                    $scope.taxChartdata = emptyChart;
                 });
 
             }
 
+            if(fromDate!=null&&toDate!=null) {
 
+                if (fromDate > toDate) {
+
+                    toastr.error('Invalid Date Range', 'Warning', {
+                        closeButton: true
+                    });
+                } else {
+
+                    var postData = {
+                        appId: $rootScope.appId,
+                        fromDate: data.fromDate,
+                        toDate: data.toDate
+                    }
+
+                    commerceService.getTaxSummary(postData)
+                        .success(function (result) {
+
+                            $scope.tax_total = 0;
+                            $scope.taxList = result.data;
+
+                            angular.forEach($scope.taxList, function (tax) {
+                                $scope.tax_total = $scope.tax_total + tax.taxTotal;
+                            })
+
+                        }).error(function (error) {
+                        toastr.error('Loading Error', 'Warning', {
+                            closeButton: true
+                        });
+                    });
+
+                    commerceService.getChartData(postData)
+                        .success(function (result) {
+                            $scope.taxChartdata = result.data;
+                        }).error(function (error) {
+                        console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                        $scope.taxChartdata = emptyChart;
+                    });
+
+                }
+
+            }
 
         };
 
         $scope.getShippingSummary = function (data) {
 
-            if (data.fromDate > data.toDate){
+            var fromDate ;
+            var toDate ;
+            var selectedData;
+            $scope.shpping_total=null;
+            $scope.salesChartdata =null;
+            $scope.shppingList =null;
+            $scope.shippingChartdata =null;
 
-                toastr.error('Invalid Date Range', 'Warning', {
+            try{
+                data.fromDate.toString().length;
+                data.toDate.toString().length;
+                fromDate = data.fromDate;
+                toDate = data.toDate;
+
+            }catch(e){
+                toastr.error('Please fill all fields', 'Warning', {
                     closeButton: true
                 });
-            }else {
 
-                var postData = {
-                    appId: $rootScope.appId,
-                    fromDate: data.fromDate,
-                    toDate: data.toDate
-                }
+            }
 
-                commerceService.getShippingSummary(postData)
-                    .success(function (result) {
+            if(fromDate!=null&&toDate!=null) {
 
-                        $scope.shpping_total = 0;
-                        $scope.shppingList = result.data;
-                        $scope.url = "/reports/getOrderData?appId=" + $rootScope.appId + "&fromDate=" + data.fromDate + "&toDate=" + data.toDate;
+                if (fromDate > toDate) {
 
-                        angular.forEach($scope.shppingList, function (tax) {
-                            $scope.shpping_total = $scope.shpping_total + tax.totalShippingCost;
-                        })
-
-                    }).error(function (error) {
-                    toastr.error('Loading Error', 'Warning', {
+                    toastr.error('Invalid Date Range', 'Warning', {
                         closeButton: true
                     });
-                });
+                } else {
 
-                commerceService.getChartData(postData)
-                    .success(function (result) {
-                        $scope.shippingChartdata = result.data;
-                    }).error(function (error) {
-                    console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
-                    $scope.shippingChartdata = emptyChart;
-                });
+                    var postData = {
+                        appId: $rootScope.appId,
+                        fromDate: data.fromDate,
+                        toDate: data.toDate
+                    }
+
+                    commerceService.getShippingSummary(postData)
+                        .success(function (result) {
+
+                            $scope.shpping_total = 0;
+                            $scope.shppingList = result.data;
+                            $scope.url = "/reports/getOrderData?appId=" + $rootScope.appId + "&fromDate=" + data.fromDate + "&toDate=" + data.toDate;
+
+                            angular.forEach($scope.shppingList, function (tax) {
+                                $scope.shpping_total = $scope.shpping_total + tax.totalShippingCost;
+                            })
+
+                        }).error(function (error) {
+                        toastr.error('Loading Error', 'Warning', {
+                            closeButton: true
+                        });
+                    });
+
+                    commerceService.getChartData(postData)
+                        .success(function (result) {
+                            $scope.shippingChartdata = result.data;
+                        }).error(function (error) {
+                        console.log("Error on commerceService.getChartData \n" + JSON.stringify(error, null, 2));
+                        $scope.shippingChartdata = emptyChart;
+                    });
+                }
             }
         };
 
         $scope.downloadFile = function (data,type) {
 
-            if (!data.fromDate || !data.toDate) {
+            var fromDate;
+            var toDate;
+            var selectedData;
+            $scope.total = null;
+            $scope.salesChartdata = null;
+            $scope.salesList = null;
 
-                toastr.error('Please select the date range', 'Warning', {
-                    closeButton: true
-                });
-            } else {
+            try {
 
-                var paramData;
-
-                if (type == 'sales') {
-
-                    paramData = {
-                        appId: $rootScope.appId,
-                        fromDate: data.fromDate,
-                        toDate: data.toDate,
-                        selectedProducts: data.selectedProducts,
-                        type: type
-                    }
-                } else {
-                    paramData = {appId: $rootScope.appId, fromDate: data.fromDate, toDate: data.toDate, type: type};
+                fromDate = data.fromDate;
+                toDate = data.toDate;
+                selectedData = data.selectedProducts;
+                if (selectedData.length < 1) {
+                    toastr.error('Please fill all fields', 'Warning', {
+                        closeButton: true
+                    });
                 }
 
-                $http({
-                    method: 'POST',
-                    url: '/reports/getOrderData',
-                    data: paramData,
-                    responseType: 'arraybuffer'
-                }).success(function (data, status, headers) {
-                    headers = headers();
-
-                    var filename = headers['x-filename'];
-                    var contentType = headers['content-type'];
-
-                    var linkElement = document.createElement('a');
-                    try {
-                        var blob = new Blob([data], {type: contentType});
-                        var url = window.URL.createObjectURL(blob);
-
-                        linkElement.setAttribute('href', url);
-                        linkElement.setAttribute("download", filename);
-
-                        var clickEvent = new MouseEvent("click", {
-                            "view": window,
-                            "bubbles": true,
-                            "cancelable": false
-                        });
-                        linkElement.dispatchEvent(clickEvent);
-                    } catch (ex) {
-                        console.log(ex);
-                    }
-                }).error(function (data) {
-                    console.log(data);
+            } catch (e) {
+                toastr.error('Please fill all fields', 'Warning', {
+                    closeButton: true
                 });
+
             }
-            ;
+
+            if (fromDate && toDate && selectedData.length > 0) {
+
+                if (fromDate > toDate) {
+
+                    toastr.error('Please select the date range', 'Warning', {
+                        closeButton: true
+                    });
+                } else {
+
+                    var paramData;
+
+                    if (type == 'sales') {
+
+                        paramData = {
+                            appId: $rootScope.appId,
+                            fromDate: data.fromDate,
+                            toDate: data.toDate,
+                            selectedProducts: data.selectedProducts,
+                            type: type
+                        }
+                    } else {
+                        paramData = {appId: $rootScope.appId, fromDate: data.fromDate, toDate: data.toDate, type: type};
+                    }
+
+                    $http({
+                        method: 'POST',
+                        url: '/reports/getOrderData',
+                        data: paramData,
+                        responseType: 'arraybuffer'
+                    }).success(function (data, status, headers) {
+                        headers = headers();
+
+                        var filename = headers['x-filename'];
+                        var contentType = headers['content-type'];
+
+                        var linkElement = document.createElement('a');
+                        try {
+                            var blob = new Blob([data], {type: contentType});
+                            var url = window.URL.createObjectURL(blob);
+
+                            linkElement.setAttribute('href', url);
+                            linkElement.setAttribute("download", filename);
+
+                            var clickEvent = new MouseEvent("click", {
+                                "view": window,
+                                "bubbles": true,
+                                "cancelable": false
+                            });
+                            linkElement.dispatchEvent(clickEvent);
+                        } catch (ex) {
+                            console.log(ex);
+                        }
+                    }).error(function (data) {
+                        console.log(data);
+                    });
+                }
+                ;
+            }
         }
     }
 
