9c03cd9675533b339f4844c2a12095469ce8caaf
OnbitShashan <shashan@onbitlabs.com>
2018-02-19 12:23:35 +0530

    ME-2734 added stagin env to master

--- source/config/env/stage.js
+++ source/config/env/stage.js
@@ -1,34 +1,57 @@
 /**
- * Created by amila on 8/11/16.
+ * Production environment settings
+ *
+ * This file can include shared settings for a production environment,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
+ *
  */
 
-/**
- * stage environment settings
- *
- * This is custom stage environment setting 
- *
- * */
-
 module.exports = {
-    
-    models: {
-        connection: 'developmentMongodbServer'
-    },
-    port: 1337,
-    log: {
-        level: "debug"
-    },
+  hookTimeout: 90000,
+  /***************************************************************************
+   * Set the default database connection for models in the production        *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
 
-    /** ****** Custom Config ****** **/
-    // HOST URL
-    HOST_URL : 'http://192.168.8.56:1337',
-    // ME SERVER Config
-    ME_SERVER_URL : 'http://192.168.8.56:80/temp/',
-    ME_SERVER : '/home/admin/web/otenro.meserver.local.com/public_html/temp/',
-    ME_SERVER_PORT : 80,
-    // APP FILE SERVER Config
-    APP_FILE_SERVER : '/home/otenro/appFileServer/',
-    CLIENT_SECRET : 'stage44f4f3be572ec33711a40a5b8b4789',
-    TEMPLATES_PATH : '/var/lib/go-agent/pipelines/OtenroPipeLine/initData/templates/',
-    REDIRECT_URL : 'http://192.168.8.56:1337'
+  models: {
+    connection: 'productionMongodbServer'
+  },
+
+  /***************************************************************************
+   * Set the port in the production environment to 80                        *
+   ***************************************************************************/
+
+  port: 1448,
+
+  ssl: {
+    ca: require('fs').readFileSync(__dirname + '/ssl/ca_bundle.crt'),
+    key: require('fs').readFileSync(__dirname + '/ssl/private.key'),
+    cert: require('fs').readFileSync(__dirname + '/ssl/certificate.crt')
+  },
+
+  /***************************************************************************
+   * Set the log level in production environment to "silent"                 *
+   ***************************************************************************/
+
+  log: {
+    level: "silent"
+  },
+
+  /** ****** Custom Config ****** **/
+  // HOST URL
+  HOST_URL : 'https://testdashboard.otenro.com',
+  // ME SERVER Config
+  ME_SERVER_URL : 'https://testcdn.otenro.com/temp/',
+  ME_SERVER : '/home/admin/web/testcdn.otenro.com/public_html/temp/',
+  ME_SERVER_PORT : 8081,
+  // APP FILE SERVER Config
+  APP_FILE_SERVER : '/home/otenro/OtenroTest/appFileServer/',
+  CLIENT_SECRET : 'pro44f4f3be572ec33711a40a5b8b4789',
+  TEMPLATES_PATH : '/home/otenro/OtenroTest/Otenro/initData/templates/',
+  REDIRECT_URL : 'https://testdashboard.otenro.com:1448'
+
 };
