cc01d96a6f3425704b6c64dc22550460c8ddbd28
OnbitShashan <shashan@onbitlabs.com>
2018-02-20 17:43:47 +0530

    ME-2800 stage meserver url

--- source/assets/env/stage.js
+++ source/assets/env/stage.js
@@ -2,4 +2,4 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'https://testdashboard.otenro.com:1448/';
-O_MESERVER_URL = 'https://testcdn.otenro.com/';
+O_MESERVER_URL = '/home/admin/web/testcdn.otenro.com/public_html/';
