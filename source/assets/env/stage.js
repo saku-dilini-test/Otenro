5f1acd245566d4dde48121d6ea4adc7b39fbb121
OnbitShashan <shashan@onbitlabs.com>
2018-02-20 14:58:11 +0530

    ME-2800 changed assests env stage file o_server_url

--- source/assets/env/stage.js
+++ source/assets/env/stage.js
@@ -1,5 +1,5 @@
 /**
  * Created by praveen on 3/2/17.
  */
-O_SERVER_URL = 'https://testdashboard.otenro.com/';
+O_SERVER_URL = 'https://testdashboard.otenro.com:1448/';
 O_MESERVER_URL = 'https://testcdn.otenro.com/temp/';
