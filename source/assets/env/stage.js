a854a75b112e937bd73f103f0ac7707cfc610eb3
OnbitShashan <shashan@onbitlabs.com>
2018-02-20 15:09:11 +0530

    ME-2800 o_server_url removed temp

--- source/assets/env/stage.js
+++ source/assets/env/stage.js
@@ -2,4 +2,4 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'https://testdashboard.otenro.com:1448/';
-O_MESERVER_URL = 'https://testcdn.otenro.com/temp/';
+O_MESERVER_URL = 'https://testcdn.otenro.com/';
