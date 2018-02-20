d5be859b3417ef89805ae06529d7eb8eaaa28976
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-02-20 15:40:00 +0530

    ME-2772 Fixed undefined columns issue in downloaded reports of sales, tax and shipping in analytics.

--- source/api/controllers/reports/StoreAnalyticsController.js
+++ source/api/controllers/reports/StoreAnalyticsController.js
@@ -259,9 +259,21 @@ module.exports = {
                 reportData.push(" , "+"Country"+","+ "Customer" +"," +
                     "Order ID" + "," +  "Date" + ","+ "Amount"+ ","+"Tax"+'\r\n');
                 Order.forEach(function(order) {
-                    var data = order.deliveryCountry+","+order.customerName +
-                        ","+ order.id + ","+ order.updatedAt+","+order.amount+","+order.tax+'\r\n';
-                    reportData.push(data);
+
+                    var data = null;
+                    if (order.shippingOpt != null) {
+                        if (order.shippingOpt == "Weight Based" || order.shippingOpt == "Flat Rate"){
+                            data = order.deliveryCountry+","+order.customerName +
+                                ","+ order.id + ","+ order.updatedAt+","+order.amount+","+order.tax+'\r\n';
+                        }
+                    }
+                    else if (order.pickUp != null){
+                            data = order.pickUp.country+","+order.customerName +
+                            ","+ order.id + ","+ order.updatedAt+","+order.amount+","+order.tax+'\r\n';
+                    }
+                    if (data != null){
+                        reportData.push(data);
+                    }
                 });
 
             }else if (type=='shipping'){
@@ -272,16 +284,28 @@ module.exports = {
                     "Date" + ","+ "Amount"+"," +"Shipping Type" +","+ "Shipping Cost" +'\r\n');
                 Order.forEach(function(order) {
 
-                            var data = order.deliveryCountry+","+order.customerName + ","+
+                    var data = null;
+                    if (order.shippingOpt != null) {
+                        if (order.shippingOpt == "Weight Based" || order.shippingOpt == "Flat Rate"){
+                            data = order.deliveryCountry+","+order.customerName + ","+
                                 order.id + ","+ order.updatedAt+","+order.amount+","+
                                 order.shippingOpt+","+order.shippingCost+'\r\n';
-                            reportData.push(data);
+                        }
+                    }
+                    else if (order.pickUp != null){
+                        data = order.pickUp.country+","+order.customerName + ","+
+                            order.id + ","+ order.updatedAt+","+order.amount+","+
+                            order.pickUp.shippingOption+","+order.pickUp.cost+'\r\n';
+                    }
+                    if (data != null){
+                        reportData.push(data);
+                    }
+
                 });
 
             }else if (type=='sales'){
 
                 //sales
-
                 reportData.push(" , "+"Country"+","+ "Customer" +"," + "Product" + "," +
                     "Date" + ","+ "Amount"+","+ "SKU"+"," +"variant"  +'\r\n');
                 Order.forEach(function(order) {
@@ -289,10 +313,23 @@ module.exports = {
                         if(selectedProductData.indexOf(item.name) > -1){
                             var itemTotal = item.qty * item.price;
                             item.variant.forEach(function (variant) {
-                                var data = order.deliveryCountry+","+order.customerName + ","+
-                                    item.name + ","+ order.updatedAt+","+itemTotal+","+item.sku+"," +
-                                    ""+variant.name+"-"+variant.vType+'\r\n';
-                                reportData.push(data);
+
+                                var data = null;
+                                if (order.shippingOpt != null) {
+                                    if (order.shippingOpt == "Weight Based" || order.shippingOpt == "Flat Rate"){
+                                        data = order.deliveryCountry+","+order.customerName + ","+
+                                            item.name + ","+ order.updatedAt+","+itemTotal+","+item.sku+"," +
+                                            ""+variant.name+"-"+variant.vType+'\r\n';
+                                    }
+                                }
+                                else if (order.pickUp != null){
+                                        data = order.pickUp.country+","+order.customerName + ","+
+                                        item.name + ","+ order.updatedAt+","+itemTotal+","+item.sku+"," +
+                                        ""+variant.name+"-"+variant.vType+'\r\n';
+                                }
+                                if (data != null){
+                                    reportData.push(data);
+                                }
                             })
                         }
                     })
