2007bfb607c4e9d36862759930a844b5a8383471
OnbitShashan <shashan@onbitlabs.com>
2018-02-22 13:31:49 +0530

    ME-2823 fixed social media addresses

--- initData/progressiveTemplates/quantum/src/app/addons/contact-info/contact-info.component.ts
+++ initData/progressiveTemplates/quantum/src/app/addons/contact-info/contact-info.component.ts
@@ -1,60 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SERVER_URL } from '../../constantsService';
+import { Component, Input } from '@angular/core';
 import * as data from '../../madeEasy.json';
-import { AppDataService } from '../../services/appdata-info/appdata-info.service';
 @Component({
 	selector: 'app-contact-info',
 	templateUrl: './app/addons/contact-info/contact-info.component.html',
 	styleUrls: ['./app/addons/contact-info/contact-info.component.css']
 })
-export class ContactInfoComponent implements OnInit {
+export class ContactInfoComponent {
 
 	private appId = (<any>data).appId;
 	private userId = (<any>data).userId;
-	private address;
-	private email;
-	private webSite;
-	private telPhone;
-	private description;
-	facebook = 'www.facebook.com';
-	twitter = 'www.twitter.com';
-	pinterest = 'www.pinterest.com';
-	linkedin = "www.linkedin.com"
-	// @Input('contact') contactInfo:contactInfoModel;
+	 @Input('contacts') contacts:contactInfoModel;
 
-	constructor(private appdataService: AppDataService) {
-		this.appdataService.getContactUs().subscribe((data: any) => {
-
-			// console.log("contact data: " + JSON.stringify(data));
-			this.description = ""
-			this.address = data.address;
-			this.email = data.email;
-			this.website = data.webSite;
-			this.phone = data.telPhone;
-			this.coords = data.coords;
-
-
-		}), ((err) => {
-			alert('warning' + " Unable to get contact us info");
-		});
-	}
-
-	ngOnInit() {
-	}
-
-
-
+	constructor() {	}
 }
-
 export class contactInfoModel {
-	description: string;
 	address: string;
-	phone: string;
+  telPhone: string;
 	email: string;
-	website: string;
+  webSite: string;
 	facebook: string;
 	twitter: string;
 	pinterest: string;
 	linkedin: string;
+	instagram: string;
 }
