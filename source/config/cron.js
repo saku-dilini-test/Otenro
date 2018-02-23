bb8ace79b3136b2e24c0843084a040794e79b800
dilakshandc <dilakshan@onbitlabs.com>
2018-02-23 16:50:27 +0530

    ME-2765 crone service for email reminder

---
+++ source/config/cron.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Praveen on 5/15/17.
+ */
+
+module.exports.cron = {
+//    myFirstJob: {
+//        schedule: '00 00 01 * * *',
+//        //schedule: '*/10 * * * * *',
+//        onTick: function () {
+//            if(sails){
+//                /*runs everyday at 1 am*/
+//                sails.log.debug("sending reminder emails");
+//                var EmailService = require('../api/services/emailService.js');
+//                EmailService.sendReminderEmail();
+//                sails.log.debug("sending reminder emails - completed");
+//            }
+//        }
+//    }
+};
