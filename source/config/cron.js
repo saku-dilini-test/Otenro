d2d075881cb0e00121ce57b04906d63bdd398fa3
dilakshandc <dilakshan@onbitlabs.com>
2018-02-23 17:28:13 +0530

    ME-2765 email reminder service

--- source/config/cron.js
+++ source/config/cron.js
@@ -3,17 +3,17 @@
  */
 
 module.exports.cron = {
-    myFirstJob: {
-        schedule: '00 00 01 * * *',
-        //schedule: '*/10 * * * * *',
-        onTick: function () {
-            if(sails){
-                /*runs everyday at 1 am*/
-                sails.log.debug("sending reminder emails");
-                var EmailService = require('../api/services/emailService.js');
-                EmailService.sendReminderEmail();
-                sails.log.debug("sending reminder emails - completed");
-            }
-        }
-    }
+//    myFirstJob: {
+//        schedule: '00 00 01 * * *',
+//        //schedule: '*/10 * * * * *',
+//        onTick: function () {
+//            if(sails){
+//                /*runs everyday at 1 am*/
+//                sails.log.debug("sending reminder emails");
+//                var EmailService = require('../api/services/emailService.js');
+//                EmailService.sendReminderEmail();
+//                sails.log.debug("sending reminder emails - completed");
+//            }
+//        }
+//    }
 };
