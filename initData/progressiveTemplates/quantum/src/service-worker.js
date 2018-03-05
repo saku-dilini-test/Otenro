9a2c19f175ff2bfe3f870b404ddbb81c58774d6b
OnbitShashan <shashan@onbitlabs.com>
2018-03-05 08:48:37 +0530

    ME-2884 service worker 2 ways

--- initData/progressiveTemplates/quantum/src/service-worker.js
+++ initData/progressiveTemplates/quantum/src/service-worker.js
@@ -173,3 +173,138 @@ const PRECACHE_URLS = [
 //     self.registration.showNotification('Push Notification', options)
 //   );
 // });
+
+ //
+ // importScripts('../../../../../node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.3.js');
+ //
+ // const workboxSW = new WorkboxSW();
+ // workboxSW.precache([
+ //   {
+ //     "url": "./styles.css",
+ //     "revision": "3a78f101efdbf4c896cef53c323c7bb7"
+ //   },
+ //   {
+ //     "url": "./index.html",
+ //     "revision": "ee7d4366f82a736863dc612c50d16e54"
+ //   },
+ //   {
+ //     "url": "./systemjs.config.js",
+ //     "revision": "ee7d4366f82a736863dc612c50d16e55"
+ //   },
+ //   {
+ //     "url": "../../../../../node_modules/workbox-sw/build/importScripts/workbox-sw.prod.v2.1.3.js",
+ //     "revision": "9029a00430d1c6ccf363f3ad77c45d42"
+ //   },
+ //   {
+ //     "url": "../../../../../node_modules/lodash/lodash.js",
+ //     "revision": "9029a00430d1c6ccf363f3ad77c45d43"
+ //   },
+ //   {
+ //     "url": "../../../../../node_modules/@angular/compiler/bundles/compiler.umd.js ",
+ //     "revision": "9029a00430d1c6ccf363f3ad77c45d44"
+ //   },
+ //   './assets/css/animate.min.css',
+ //   './assets/css/bootstrap.min.css',
+ //   './assets/css/feather.min.css',
+ //   './assets/css/font-awesome.min.css',
+ //   './assets/icons/icon-72x72.png',
+ //   './assets/icons/icon-96x96.png',
+ //   './assets/icons/icon-128x128.png',
+ //   './assets/icons/icon-144x144.png',
+ //   './assets/icons/icon-152x152.png',
+ //   './assets/icons/icon-192x192.png',
+ //   './assets/icons/icon-384x384.png',
+ //   './assets/icons/icon-512x512.png',
+ //   './assets/images/secondNavi/1.JPG',
+ //   './assets/images/secondNavi/2.JPG',
+ //   './assets/images/secondNavi/3.JPG',
+ //   './assets/images/secondNavi/4.JPG',
+ //   './assets/images/secondNavi/5.JPG',
+ //   './assets/images/slider/slider1.png',
+ //   './assets/images/slider/slider2.png',
+ //   './assets/images/slider/slider3.png',
+ //   './assets/images/thirdNavi/11.JPG',
+ //   './assets/images/thirdNavi/12.JPG',
+ //   './assets/images/thirdNavi/13.JPG',
+ //   './assets/images/thirdNavi/14.JPG',
+ //   './assets/images/thirdNavi/15.JPG',
+ //   './assets/images/thirdNavi/16.JPG',
+ //   './assets/images/thirdNavi/17.JPG',
+ //   './assets/images/thirdNavi/18.JPG',
+ //   './assets/images/thirdNavi/19.JPG',
+ //   './assets/images/thirdNavi/20.JPG',
+ //   './assets/images/thirdNavi/21.JPG',
+ //   './assets/images/thirdNavi/22.jpg',
+ //   './assets/images/thirdNavi/23.jpg',
+ //   './assets/images/thirdNavi/24.jpg',
+ //   './assets/images/thirdNavi/25.jpg',
+ //   './assets/js/blocs.min.js',
+ //   './assets/js/bootstrap.min.js',
+ //   './assets/js/formHandler.js',
+ //   './assets/js/jqBootstrapValidation.js',
+ //   './assets/js/jquery.touchSwipe.min.js',
+ //   './assets/js/jquery-2.1.0.min.js',
+ //   './assets/js/lazysizes.min.js',
+ //   './assets/js/shim.min.js',
+ //   './assets/js/system.src.js',
+ //   './assets/js/zone.js',
+ //   './main.ts',
+ // ]);
+ //
+ // workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'googleapis',
+ //     cacheExpiration: {
+ //       maxEntries: 20
+ //     },
+ //     cacheableResponse: {statuses: [0, 200]}
+ //   })
+ // );
+ //
+ // workboxSW.router.registerRoute('https://unpkg.com/(.*)',
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'unpkg',
+ //     cacheExpiration: {
+ //       maxEntries: 10
+ //     },
+ //     cacheableResponse: {statuses: [0, 200]}
+ //   })
+ // );
+ //
+ // workboxSW.router.registerRoute('http://weloveiconfonts.com/(.*)',
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'iconfonts',
+ //     cacheExpiration: {
+ //       maxEntries: 20
+ //     },
+ //     cacheableResponse: {statuses: [0, 200]}
+ //   })
+ // );
+ // workboxSW.router.registerRoute('http://localhost/meServer/node_modules/(.*)',
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'node_modules',
+ //     cacheExpiration: {
+ //       maxEntries: 100
+ //     },
+ //     cacheableResponse: {statuses: [0, 200]}
+ //   })
+ // );
+ // workboxSW.router.registerRoute('http://localhost:1337/(.*)',
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'template_cache',
+ //     cacheExpiration: {
+ //       maxEntries: 100
+ //     },
+ //     cacheableResponse: {statuses: [0, 200]}
+ //   })
+ // );
+ //
+ // // We want no more than 50 images in the cache. We check using a cache first strategy
+ // workboxSW.router.registerRoute(/\.(?:png|gif|jpg|JPG|PNG)$/,
+ //   workboxSW.strategies.cacheFirst({
+ //     cacheName: 'images-cache',
+ //     cacheExpiration: {
+ //       maxEntries: 50
+ //     }
+ //   })
+ // );
