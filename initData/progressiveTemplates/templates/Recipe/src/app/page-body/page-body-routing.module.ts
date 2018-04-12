601fa6648e8c50a333fc8a0047b298ec34152ca3
OnbitShashan <shashan@onbitlabs.com>
2018-04-12 10:01:30 +0530

    ID-9 added recipe app

---
+++ initData/progressiveTemplates/templates/Recipe/src/app/page-body/page-body-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { AppUserComponent } from './app-user/app-user.component';
+
+const routes: Routes = [
+ { path: '',
+   component: HomepageComponent,
+    pathMatch: 'full'
+  },
+ {
+    path: 'aboutus',
+    component: AboutusComponent,
+    data:{title:'About Us Page'}
+  },
+  {
+    path: 'policies',
+    component: PoliciesComponent,
+    data:{title:'Policies Page'}
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data:{title:'Contact Page'}
+  },
+  {
+    path: 'shop/:id/:name',
+    component: ShopComponent,
+    data:{title:'Shop Page'}
+  },
+  {
+    path: 'product/:catName',
+    component: ProductComponent,
+    data:{title:'Product Page'}
+  },
+// {
+//   path: 'paypal',
+// component: PaypalPaymentComponent
+// }
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class PageBodyRoutingModule { }
---
+++ initData/progressiveTemplates/templates/Recipe/src/app/services/categories/categories.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs/Rx";
+import { SERVER_URL } from '../../constantsService';
+import * as data from '../../madeEasy.json';
+
+@Injectable()
+export class CategoriesService {
+
+  public appId = (<any>data).appId;
+  public userId = (<any>data).userId;
+  result: any;
+
+  constructor(private http: Http) { }
+		// '/templates/getArticleCategoryByAppId?appId='
+		// '/templates/getSpecificChild?appId='
+  getCategories() {
+    return this.http.get(SERVER_URL+'/templates/getArticleCategoryByAppId?appId=' + this.appId)
+                    .map(res => res.text() ? res.json() : null);
+  }
+
+}
+
+
+
+
