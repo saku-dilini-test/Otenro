748e68a7a652e306ab3e5ec00efb12740261203e
prasannaonbitlabs <prasanna@onbitlabs.com>
2018-04-25 18:48:48 +0530

    ID-6 Push Messages

--- initData/progressiveTemplates/templates/Recipe/src/app/page-body/homepage/homepage.component.ts
+++ initData/progressiveTemplates/templates/Recipe/src/app/page-body/homepage/homepage.component.ts
@@ -5,70 +5,98 @@ import * as data from '../../madeEasy.json';
 import { CategoriesService } from '../../services/categories/categories.service'
 import { PagebodyServiceModule } from '../../page-body/page-body.service'
 import { TitleService } from '../../services/title.service';
+import {CordovaPluginFirebaseService} from "../../services/cordova-plugin-services/cordova-plugin-firebase.service";
+
+var homePageCmp;
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './app/page-body/homepage/homepage.component.html',
-  styleUrls: ['./app/page-body/homepage/homepage.component.css'],
+    selector: 'app-homepage',
+    templateUrl: './app/page-body/homepage/homepage.component.html',
+    styleUrls: ['./app/page-body/homepage/homepage.component.css'],
 
 })
 export class HomepageComponent implements OnInit {
 
-  private appId = (<any>data).appId;
-  private userId = (<any>data).userId;
-  private categoryId;
-  private categoryName;
-  private imageUrl: any;
-  private products: any;
-  private results: {};
-  private randomIndex;
-  private imageUrlProd;
-  private randomedArr = [];
-  private sliderData: any;
-  private imageUrlSlider;
-  private catName;
-  private isSliderDataAvailable: boolean = false;
-  private isRandomProducts;
+    private appId = (<any>data).appId;
+    private userId = (<any>data).userId;
+    private categoryId;
+    private categoryName;
+    private imageUrl: any;
+    private products: any;
+    private results: {};
+    private randomIndex;
+    private imageUrlProd;
+    private randomedArr = [];
+    private sliderData: any;
+    private imageUrlSlider;
+    private catName;
+    private isSliderDataAvailable: boolean = false;
+    private isRandomProducts;
 
-  constructor(private route: Router, private dataService: PagebodyServiceModule, private router: Router, private categoryService: CategoriesService, private title: TitleService) {
+    constructor(private route: Router, private dataService: PagebodyServiceModule,
+                private router: Router, private categoryService: CategoriesService,
+                private title: TitleService,private  push: CordovaPluginFirebaseService) {
 
-    this.title.changeTitle("Your Horoscope");
+        this.title.changeTitle("Your Horoscope");
+        homePageCmp = this;
+    }
 
-  }
+    ngOnInit() {
+        this.generatePushToken();
 
+        this.imageUrl = SERVER_URL + "/templates/viewWebImages?userId="
+            + this.userId + "&appId=" + this.appId + "&" + new Date().getTime() + "&images=secondNavi";
 
-
-  ngOnInit() {
-
-    this.imageUrl = SERVER_URL + "/templates/viewWebImages?userId="
-      + this.userId + "&appId=" + this.appId + "&" + new Date().getTime() + "&images=secondNavi";
-
-    this.categoryService.getCategories().subscribe(data => {
-      if (data.length > 0) {
-        // Read the result field from the JSON response.
-        this.results = data;
-        this.dataService.searchArray = [];
-        data.forEach(element => {
-          this.dataService.searchArray.push({ 'name': element.name, 'id': element.id });
-        });
-      } else {
-        this.results = null;
-      }
-
-
-    },
-      error => {
-        console.log('Error retrieving categories');
-      });
-
-  }
+        this.categoryService.getCategories().subscribe(data => {
+                if (data.length > 0) {
+                    // Read the result field from the JSON response.
+                    this.results = data;
+                    this.dataService.searchArray = [];
+                    data.forEach(element => {
+                        this.dataService.searchArray.push({ 'name': element.name, 'id': element.id });
+                    });
+                } else {
+                    this.results = null;
+                }
 
 
-  // Routing Method
-  navigateShop(val: string, id, name,image) {
-    this.dataService.catId = id;
-    this.router.navigate(['/' + val, id, name,image]);
-  }
+            },
+            error => {
+                console.log('Error retrieving categories');
+            });
 
- 
+    }
+
+
+    // Routing Method
+    navigateShop(val: string, id, name) {
+        this.dataService.catId = id;
+        this.router.navigate(['/' + val, id, name]);
+    }
+
+    pushSuccessCallback(results: any){
+        alert(results);
+
+        try {
+            homePageCmp.categoryService.sendDeviceToken(results).subscribe(data => {
+                    //console.log();
+                    alert(data);
+                },
+                error => {
+                    alert('Error retrieving categories');
+                });
+        }catch(err){
+            alert(err);
+        }
+    }
+
+    pushErrorCallback(error: any){
+        alert("pushErrorCallback=>" + error);
+    }
+
+    generatePushToken(){
+        this.push.getToken(this.pushSuccessCallback, this.pushErrorCallback);
+    }
+
+
 }
