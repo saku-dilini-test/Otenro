06bb9636d6870aaa2606aa403867d099612e655a
dilakshandc <dilakshan@onbitlabs.com>
2018-05-30 17:07:29 +0530

    ME-3072 returning customer outfit register page

--- initData/progressiveTemplates/templates/outfit/src/app/page-body/register/register.component.ts
+++ initData/progressiveTemplates/templates/outfit/src/app/page-body/register/register.component.ts
@@ -127,8 +127,12 @@ export class RegisterComponent implements OnInit {
 
                 if(this.navigate == 'home'){
                     this.route.navigate(['home']);
-                }else{
+                }else if(this.navigate == 'cart'){
                   this.route.navigate(['cart']);
+                }else if(this.navigate == 'delivery'){
+                  this.route.navigate(['checkout','delivery']);
+                }else{
+                  this.route.navigate(['checkout','pickup']);
                 }
             },err =>{
 
