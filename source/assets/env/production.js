35cd0d7b8aea4100012e556b98cac2d9cf1d8dc9
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-06-06 17:18:33 +0530

    ID-95 Path Changes

--- source/assets/env/production.js
+++ source/assets/env/production.js
@@ -2,5 +2,5 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'http://developer.appmaker.lk:1338/';
-O_MESERVER_URL = 'http://developer.appmaker.lk:80/';
+O_MESERVER_URL = 'http://developer.appmaker.lk:80/meServer/';
 O_ME_SERVER = '/home/milhan/public_html/developer/meServer/temp/';
