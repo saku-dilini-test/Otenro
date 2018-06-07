e4f6334ed99634672c470c01878bda08dcc92ab7
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-06-07 15:15:00 +0530

    ID-95 Ideadroid Change domain to ip in production

--- source/assets/env/production.js
+++ source/assets/env/production.js
@@ -1,6 +1,6 @@
 /**
  * Created by praveen on 3/2/17.
  */
-O_SERVER_URL = 'http://developer.appmaker.lk/';
+O_SERVER_URL = 'http://173.82.243.121:1338/';
 O_MESERVER_URL = 'http://developer.appmaker.lk/meServer/';
 O_ME_SERVER = '/home/milhan/public_html/developer/meServer/temp/';
