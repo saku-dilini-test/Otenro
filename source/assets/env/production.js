fd76fa43714774f17f16eecdb05067d8b9b8088a
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-06-11 12:02:54 +0530

    ID-95 Ideadroid apache port change to 80 in assets/env/production.js

--- source/assets/env/production.js
+++ source/assets/env/production.js
@@ -2,5 +2,5 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'http://173.82.243.121:1338/';
-O_MESERVER_URL = 'http://developer.appmaker.lk:8181/meServer/';
+O_MESERVER_URL = 'http://developer.appmaker.lk:80/meServer/';
 O_ME_SERVER = '/home/milhan/public_html/developer/meServer/temp/';
