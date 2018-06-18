1dbaa4bbe88f45c806353bc37fce0bce5a51e445
OnbitShashan <shashan@onbitlabs.com>
2018-06-18 18:45:04 +0530

    ME-3098 smartfit enhansements

--- source/config/env/stage.js
+++ source/config/env/stage.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1338,
+  port: 1337,
 
   ssl: {
     ca: require('fs').readFileSync(__dirname + '/ssl/ca_bundle.crt'),
