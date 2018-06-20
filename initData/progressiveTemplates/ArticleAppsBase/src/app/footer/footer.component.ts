6038b2cc1db302d00038cc77101705e7271380c0
dilakshandc <dilakshan@onbitlabs.com>
2018-06-20 15:15:04 +0530

    ID-16 user subscription on app changes

--- initData/progressiveTemplates/ArticleAppsBase/src/app/footer/footer.component.ts
+++ initData/progressiveTemplates/ArticleAppsBase/src/app/footer/footer.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import * as data from '../madeEasy.json';
 import { AppDataService } from '../services/appdata-info/appdata-info.service';
 import { SubscribedDataService } from '../services/subscribed-data/subscribed-data.service'
+import * as data from './../madeEasy.json';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './app/footer/footer.component.html',
   styleUrls: ['./app/footer/footer.component.css']
 })
+
 export class FooterComponent{
 
   private subscriptionStatus;
+  private appId = <any>(data).appId;
+  private userId = <any>(data).userId;
 
   constructor(private subscription:SubscribedDataService,private router: Router) {
-    this.subscription.getSubscribedData().subscribe(data =>{
+
+    var msisdn = localStorage.getItem(this.appId+"msisdn");
+    let parseData = {appId:this.appId,msisdn:msisdn}
+
+    this.subscription.getSubscribedData(parseData).subscribe(data =>{
       console.log(data);
       this.subscriptionStatus = data.isSubscribed;
   })
