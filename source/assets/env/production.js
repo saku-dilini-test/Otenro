c183bc07bce64621ad43e55708b51dabf4f6a0ee
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-07-03 12:03:34 +0530

    ID-261 Appmaker production path changes to IP

--- source/assets/env/production.js
+++ source/assets/env/production.js
@@ -2,5 +2,5 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'http://173.82.219.199:1339/';
-O_MESERVER_URL = 'http://cdn.appmaker.lk/meServer/';
-O_ME_SERVER = '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/';
+O_MESERVER_URL = 'http://173.82.219.199:80/developer/meServer/';
+O_ME_SERVER = '/home/admin/web/appmaker.lk/public_html/developer/meServer/temp/';
--- source/config/env/production.js
+++ source/config/env/production.js
@@ -45,11 +45,11 @@ module.exports = {
   // HOST URL 
   HOST_URL : 'http://173.82.219.199:1339',
   // ME SERVER Config
-  ME_SERVER_URL : 'http://cdn.appmaker.lk/meServer/temp/',
-  ME_SERVER : '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/',
+  ME_SERVER_URL : 'http://173.82.219.199:80/developer/meServer/temp/',
+  ME_SERVER : '/home/admin/web/appmaker.lk/public_html/developer/meServer/temp/',
   ME_SERVER_PORT : 80,
   // APP FILE SERVER Config
-  APP_FILE_SERVER : '/home/admin/web/cdn.appmaker.lk/public_html/developer/appFileServer/',
+  APP_FILE_SERVER : '/home/admin/web/appmaker.lk/public_html/developer/appFileServer/',
   CLIENT_SECRET : 'pro44f4f3be572ec33711a40a5b8b4789',
   TEMPLATES_PATH : '/home/projects/appmaker/Otenro/initData/templates/',
   PROGRESSIVE_TEMPLATES_PATH : '/home/projects/appmaker/Otenro/initData/progressiveTemplates/',
