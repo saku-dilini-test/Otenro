8e63acc7963b8641c1ed1060a6af9c0733d50841
dilakshandc <dilakshan@onbitlabs.com>
2018-07-09 13:43:23 +0530

    ID-275 configure user new tab

--- source/config/routes.js
+++ source/config/routes.js
@@ -210,7 +210,8 @@ module.exports.routes = {
     'GET /edit/getPublishDetails': 'edit/publish/publishDetailController.getPublishDetails',
     'POST /edit/setPublishDetails': 'edit/publish/publishDetailController.setPublishDetails',
     'POST /edit/uploadPublishFiles': 'edit/publish/publishDetailController.uploadPublishFiles',
-    
+    'POST /edit/updateOperators': 'edit/publish/publishDetailController.updateOperators',
+
     'POST /edit/setContentRating': 'edit/publish/publishDetailController.setContentRating',
     'POST /edit/setAppStoreDetails': 'edit/publish/publishDetailController.setAppStoreDetails',
     'POST /edit/setAppReviewInformation': 'edit/publish/publishDetailController.setAppReviewInformation',
