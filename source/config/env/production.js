7c66e8c545e2d8f16110cbb6a7544897951244bc
chamilThushantha <chamil@simatosolutions.com>
2018-07-17 14:11:19 +0530

    ID-313 Fixed syntax error

--- source/config/env/production.js
+++ source/config/env/production.js
@@ -11,58 +11,59 @@
  */
 
 module.exports = {
-  hookTimeout: 220000,
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
+    hookTimeout: 220000,
+    /***************************************************************************
+     * Set the default database connection for models in the production        *
+     * environment (see config/connections.js and config/models.js )           *
+     ***************************************************************************/
 
-  models: {
-    connection: 'productionMongodbServer'
-  },
+    models: {
+        connection: 'productionMongodbServer'
+    },
 
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
+    /***************************************************************************
+     * Set the port in the production environment to 80                        *
+     ***************************************************************************/
 
-  port: 1339,
+    port: 1339,
 
-  ssl: {
-    ca: require('fs').readFileSync(__dirname + '/ssl/ca_bundle.crt'),
-    key: require('fs').readFileSync(__dirname + '/ssl/private.key'),
-    cert: require('fs').readFileSync(__dirname + '/ssl/certificate.crt')
-  },
+    ssl: {
+        ca: require('fs').readFileSync(__dirname + '/ssl/ca_bundle.crt'),
+        key: require('fs').readFileSync(__dirname + '/ssl/private.key'),
+        cert: require('fs').readFileSync(__dirname + '/ssl/certificate.crt')
+    },
 
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
+    /***************************************************************************
+     * Set the log level in production environment to "silent"                 *
+     ***************************************************************************/
 
-  log: {
-    level: "debug"
-  },
-  
-  /** ****** Custom Config ****** **/
-  // HOST URL 
-  HOST_URL : 'https://developer.appmaker.lk',
-  // ME SERVER Config
-  ME_SERVER_URL : 'https://cdn.appmaker.lk/developer/meServer/temp/',
-  ME_SERVER : '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/',
-  ME_SERVER_PORT : 80,
-  // APP FILE SERVER Config
-  APP_FILE_SERVER : '/home/admin/web/cdn.appmaker.lk/public_html/developer/appFileServer/',
-  CLIENT_SECRET : 'pro44f4f3be572ec33711a40a5b8b4789',
-  TEMPLATES_PATH : '/home/projects/appmaker/Otenro/initData/templates/',
-  PROGRESSIVE_TEMPLATES_PATH : '/home/projects/appmaker/Otenro/initData/progressiveTemplates/',
-  REDIRECT_URL : 'https://developer.appmaker.lk',
-  /**
-   * change isBeta to 0 after beta period
-   **/
-  isBeta: 0,
-  //IdeaBiz
-  IDEABIZ_AUTH_CONSUMER_KEY: 'fmqgPhGyJ6KFvsLe6cpgxPzlXcca',
-  IDEABIZ_AUTH_CONSUMER_SECRET: 'NZ7OQjZ2yCqsin7kxjbGpUlBsska',
-  IDEABIZ_AUTH_USERNAME: 'Appmaker',
-  IDEABIZ_AUTH_PASSWORD: 'SUperman123',
-  IDEABIZ_EMAIL: 'support@appmaker.lk',
-  IDEABIZ_GROUP_EMAIL: 'support@appmaker.lk', //To email for dialog super user, when sending approval email for the app configured with the serviceID by Admin.
-  IDEABIZ_ADMIN_EMAIL: 'communications@otenro.com' //From email address of Admin to send serviceID approval email to dialog super user.
+    log: {
+        level: "debug"
+    },
+
+    /** ****** Custom Config ****** **/
+    // HOST URL
+    HOST_URL: 'https://developer.appmaker.lk',
+    // ME SERVER Config
+    ME_SERVER_URL: 'https://cdn.appmaker.lk/developer/meServer/temp/',
+    ME_SERVER: '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/',
+    ME_SERVER_PORT: 80,
+    // APP FILE SERVER Config
+    APP_FILE_SERVER: '/home/admin/web/cdn.appmaker.lk/public_html/developer/appFileServer/',
+    CLIENT_SECRET: 'pro44f4f3be572ec33711a40a5b8b4789',
+    TEMPLATES_PATH: '/home/projects/appmaker/Otenro/initData/templates/',
+    PROGRESSIVE_TEMPLATES_PATH: '/home/projects/appmaker/Otenro/initData/progressiveTemplates/',
+    REDIRECT_URL: 'https://developer.appmaker.lk',
+    /**
+     * change isBeta to 0 after beta period
+     **/
+    isBeta: 0,
+    //IdeaBiz
+    IDEABIZ_AUTH_CONSUMER_KEY: 'fmqgPhGyJ6KFvsLe6cpgxPzlXcca',
+    IDEABIZ_AUTH_CONSUMER_SECRET: 'NZ7OQjZ2yCqsin7kxjbGpUlBsska',
+    IDEABIZ_AUTH_USERNAME: 'Appmaker',
+    IDEABIZ_AUTH_PASSWORD: 'SUperman123',
+    IDEABIZ_EMAIL: 'support@appmaker.lk',
+    IDEABIZ_GROUP_EMAIL: 'support@appmaker.lk', //To email for dialog super user, when sending approval email for the app configured with the serviceID by Admin.
+    IDEABIZ_ADMIN_EMAIL: 'communications@otenro.com' //From email address of Admin to send serviceID approval email to dialog super user.
+};
--- source/config/env/stage.js
+++ source/config/env/stage.js
@@ -12,41 +12,42 @@
 
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the development       *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
+    /***************************************************************************
+     * Set the default database connection for models in the development       *
+     * environment (see config/connections.js and config/models.js )           *
+     ***************************************************************************/
 
-  models: {
-    connection: 'developmentMongodbServer'
-  },
-  port: 1338,
-  log: {
-    level: "debug"
-  },
-  KEYWORD_LENGTH : 5,
-  /** ****** Custom Config ****** **/
-  // HOST URL
-  HOST_URL : 'http://173.82.153.215:1338',
-  // ME SERVER Config
-  ME_SERVER_URL : 'http://173.82.153.215:80/meServer/temp/',
-  ME_SERVER : '/var/www/html/meServer/temp/',
-  ME_SERVER_PORT : 80,
-  // APP FILE SERVER Config
-  APP_FILE_SERVER : '/var/www/html/meServer/temp/',
-  CLIENT_SECRET : '44f4f3be572ec33711a40a5b8b4',
-  TEMPLATES_PATH : '/home/Otenro/initData/templates/',
-  PROGRESSIVE_TEMPLATES_PATH : '/home/Otenro/initData/progressiveTemplates/',
-  REDIRECT_URL : 'http://173.82.153.215:1338',
-  /**
-   * change isBeta to 0 after beta period
-   **/
-  isBeta: 1,
+    models: {
+        connection: 'developmentMongodbServer'
+    },
+    port: 1338,
+    log: {
+        level: "debug"
+    },
+    KEYWORD_LENGTH: 5,
+    /** ****** Custom Config ****** **/
+    // HOST URL
+    HOST_URL: 'http://173.82.153.215:1338',
+    // ME SERVER Config
+    ME_SERVER_URL: 'http://173.82.153.215:80/meServer/temp/',
+    ME_SERVER: '/var/www/html/meServer/temp/',
+    ME_SERVER_PORT: 80,
+    // APP FILE SERVER Config
+    APP_FILE_SERVER: '/var/www/html/meServer/temp/',
+    CLIENT_SECRET: '44f4f3be572ec33711a40a5b8b4',
+    TEMPLATES_PATH: '/home/Otenro/initData/templates/',
+    PROGRESSIVE_TEMPLATES_PATH: '/home/Otenro/initData/progressiveTemplates/',
+    REDIRECT_URL: 'http://173.82.153.215:1338',
+    /**
+     * change isBeta to 0 after beta period
+     **/
+    isBeta: 1,
     //IdeaBiz
-  IDEABIZ_AUTH_CONSUMER_KEY: 'fmqgPhGyJ6KFvsLe6cpgxPzlXcca',
-  IDEABIZ_AUTH_CONSUMER_SECRET: 'NZ7OQjZ2yCqsin7kxjbGpUlBsska',
-  IDEABIZ_AUTH_USERNAME: 'Appmaker',
-  IDEABIZ_AUTH_PASSWORD: 'SUperman123',
-  IDEABIZ_EMAIL: 'support@appmaker.lk',
-  IDEABIZ_GROUP_EMAIL: 'support@appmaker.lk', //To email for dialog super user, when sending approval email for the app configured with the serviceID by Admin.
-  IDEABIZ_ADMIN_EMAIL: 'communications@otenro.com' //From email address of Admin to send serviceID approval email to dialog super user.
+    IDEABIZ_AUTH_CONSUMER_KEY: 'fmqgPhGyJ6KFvsLe6cpgxPzlXcca',
+    IDEABIZ_AUTH_CONSUMER_SECRET: 'NZ7OQjZ2yCqsin7kxjbGpUlBsska',
+    IDEABIZ_AUTH_USERNAME: 'Appmaker',
+    IDEABIZ_AUTH_PASSWORD: 'SUperman123',
+    IDEABIZ_EMAIL: 'support@appmaker.lk',
+    IDEABIZ_GROUP_EMAIL: 'support@appmaker.lk', //To email for dialog super user, when sending approval email for the app configured with the serviceID by Admin.
+    IDEABIZ_ADMIN_EMAIL: 'communications@otenro.com' //From email address of Admin to send serviceID approval email to dialog super user.
+};
