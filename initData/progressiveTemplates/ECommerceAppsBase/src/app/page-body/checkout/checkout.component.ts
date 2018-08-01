bbff5fe868e791fa1473bc9548044dc7757576c3
dilakshandc <dilakshan@onbitlabs.com>
2018-08-01 16:14:17 +0530

    ME-3287 pahere confirm button error

--- initData/progressiveTemplates/ECommerceAppsBase/src/app/page-body/checkout/checkout.component.ts
+++ initData/progressiveTemplates/ECommerceAppsBase/src/app/page-body/checkout/checkout.component.ts
@@ -1359,59 +1359,9 @@ export class CheckoutComponent implements OnInit {
 
       inAppBrowserRef = window.open(url, target, options);
 
-      inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
-
-      inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
-
-      inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
-
     }
 
-    function loadStartCallBack() {
 
-      $('#status-message').text("loading please wait ...");
-
-    }
-
-    function loadStopCallBack() {
-
-      if (inAppBrowserRef != undefined) {
-
-        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;" });
-
-        $('#status-message').text("");
-
-        inAppBrowserRef.show();
-      }
-
-    }
-
-    function loadErrorCallBack(params) {
-
-      $('#status-message').text("");
-
-      var scriptErrorMesssage =
-        "alert('Sorry we cannot open that page. Message from the server is : "
-        + params.message + "');"
-
-      inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-
-      inAppBrowserRef.close();
-
-      inAppBrowserRef = undefined;
-
-    }
-
-    function executeScriptCallBack(params) {
-
-      if (params[0] == null) {
-
-        $('#status-message').text(
-          "Sorry we couldn't open that page. Message from the server is : '"
-          + params.message + "'");
-      }
-
-    }
   }
 
 }
