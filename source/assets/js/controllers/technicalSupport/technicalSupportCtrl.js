6cf0edc71662d5b8614451733004789319b38d5b
dilakshandc <dilakshan@onbitlabs.com>
2018-08-22 15:26:08 +0530

    ID-415 "all" re arrange

--- source/assets/js/controllers/technicalSupport/technicalSupportCtrl.js
+++ source/assets/js/controllers/technicalSupport/technicalSupportCtrl.js
@@ -176,6 +176,33 @@
 
             }
 
+            $scope.operatorArrSort = function(operators){
+                var returnArray = [];
+                var idx;
+                var firstIdxValue;
+                $scope.sortOp = $filter('orderBy')(operators, 'operator');
+                if($scope.sortOp){
+                    for(var i = 0;i < $scope.sortOp.length; i++){
+                        if($scope.sortOp[0].operator == "all"){
+                            returnArray = $scope.sortOp;
+                            break;
+                        }else if ($scope.sortOp[0] != "all"){
+                            firstIdxValue = $scope.sortOp[0];
+                            if($scope.sortOp[i].operator == "all"){
+
+                                idx = i;
+                                $scope.sortOp[0] = $scope.sortOp[i];
+                                $scope.sortOp[i] = firstIdxValue;
+                                returnArray = $scope.sortOp;
+                                break;
+                            };
+                        }
+                    }
+                }
+                return returnArray;
+            }
+
+
             if($stateParams.adname){
                 var addname = $stateParams.adname;
 
