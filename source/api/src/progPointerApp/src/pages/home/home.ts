6ad083dd5fc23196ad099c10b5a561d3ee1934c4
dilakshandc <dilakshan@onbitlabs.com>
2018-08-29 18:05:13 +0530

    ID-513 back button disable

--- source/api/src/progPointerApp/src/pages/home/home.ts
+++ source/api/src/progPointerApp/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {DomSanitizer} from "@angular/platform-browser";
+import { IonicPage, NavParams, Platform,MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,14 +9,36 @@ import {DomSanitizer} from "@angular/platform-browser";
 })
 export class HomePage {
   private iframeSrc: any;
+  public backButtonAction: any;
+
   sanitizer: DomSanitizer;
   url: string = 'PointerAppLink';//This will be replaceed by the app url while building the apk in EditController.buildSourceProg
 
   constructor(public navCtrl: NavController,
-              sanitizer: DomSanitizer) {
+              sanitizer: DomSanitizer,public platform: Platform,public navParams: NavParams,public menu: MenuController) {
     this.sanitizer = sanitizer;
     this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.url + "?" + new Date().getTime());
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need
+
+      platform.registerBackButtonAction(() => {
+        if(this.menu.isOpen()){
+          console.log("in if");
+           this.menu.close()
+        }
+        else if(this.navCtrl.canGoBack()){
+          console.log("in else if");
+          this.navCtrl.pop();
+        }else{
+          //don't do anything
+        }
+      });
+    });
+
   }
+
 
 }
 
