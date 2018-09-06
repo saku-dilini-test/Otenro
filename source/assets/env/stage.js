163c49c9c594ad4cb79eeffc8ba614e825ad70e2
chamilThushantha <chamil@simatosolutions.com>
2018-09-06 14:30:03 +0530

    ID-489

--- source/assets/env/stage.js
+++ source/assets/env/stage.js
@@ -2,5 +2,5 @@
  * Created by praveen on 3/2/17.
  */
 O_SERVER_URL = 'http://appmaker.ddns.net/';
-O_MESERVER_URL = 'https://cdnappmaker.ddns.net/';
+O_MESERVER_URL = 'http://cdnappmaker.ddns.net/';
 O_ME_SERVER = '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/';
