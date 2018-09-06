66437275ab0d5aba189b0cc099c66662495e78e7
chamilThushantha <chamil@simatosolutions.com>
2018-09-06 14:21:33 +0530

    ID-489

--- source/assets/env/stage.js
+++ source/assets/env/stage.js
@@ -1,6 +1,6 @@
 /**
  * Created by praveen on 3/2/17.
  */
-O_SERVER_URL = 'https://testdashboard.otenro.com:1448/';
-O_MESERVER_URL = 'https://testcdn.otenro.com/';
-O_ME_SERVER = '/home/admin/web/testcdn.otenro.com/public_html/temp/';
+O_SERVER_URL = 'http://appmaker.ddns.net/';
+O_MESERVER_URL = 'https://cdnappmaker.ddns.net/';
+O_ME_SERVER = '/home/admin/web/cdn.appmaker.lk/public_html/developer/meServer/temp/';
