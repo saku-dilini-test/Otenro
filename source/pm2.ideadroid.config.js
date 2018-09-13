a1ffb43d743754c53f9b94afd24d5c96efeb5403
dilakshandc <dilakshan@onbitlabs.com>
2018-09-13 15:56:03 +0530

    ID-620 pm2 log combination.

--- source/pm2.ideadroid.config.js
+++ source/pm2.ideadroid.config.js
@@ -2,9 +2,12 @@ module.exports = {
   apps : [{
     name   : "ideadroid",
     script : "./app.js",
+    merge_logs : true,
     env:{
           NODE_ENV : "stage", "ID": "0"
         },
-    log_date_format : "DD/MM/YYYY HH:mm:ss.SSS"
+    log_date_format : "DD/MM/YYYY HH:mm:ss.SSS",
+    log: './combined.outerr.log'
+    
   }]
 }
