bbe44b6a994cc6979ff5612aa961510dc46658a4
dilakshandc <dilakshan@onbitlabs.com>
2018-10-01 15:37:19 +0530

    ID-677 pm2 for otenro

---
+++ source/pm2.otenro.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name   : "otenro",
+    script : "./app.js",
+    merge_logs : true,
+    env:{
+          NODE_ENV : "stage", "ID": "0"
+        },
+    log_date_format : "DD/MM/YYYY HH:mm:ss.SSS Z",
+    log: './combined.outerr.log'
+    
+  }]
+}
