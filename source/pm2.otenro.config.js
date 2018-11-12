e0c70a037401ec51b12645e1a2018390f270a1a2
OnbitShashan <shashan@onbitlabs.com>
2018-11-12 12:34:02 +0530

    ME-3760 Create the combined log file same as Appmaker(Dev)

--- source/pm2.otenro.config.js
+++ source/pm2.otenro.config.js
@@ -5,7 +5,10 @@ module.exports = {
     merge_logs : true,
     env:{
           NODE_ENV : "stage", "ID": "0"
-        },
+    },
+    env_production : {
+          "NODE_ENV": "production", "ID": "0"
+    },
     log_date_format : "DD/MM/YYYY HH:mm:ss.SSS Z",
     log: './combined.outerr.log'
     
