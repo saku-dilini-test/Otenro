b2420b9e5742874bcd0d15a8aadcd8e8915504a0
sudarshanaonbitlabs <sudarshana@onbitlabs.com>
2018-12-11 12:17:15 +0530

    ME-3807 - Added minimum order value field

--- source/api/models/SalesAndPromotion.js
+++ source/api/models/SalesAndPromotion.js
@@ -57,7 +57,7 @@ module.exports = {
             type:'string'
         },
         minimumOderValue : {
-            type:'string'
+            type:'float'
         },
         category : {
             type:'string'
