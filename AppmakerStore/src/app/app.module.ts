a576bc802e504aca60534eeb05281cfd92f71ddd
OnbitShashan <shashan@onbitlabs.com>
2019-01-09 15:21:40 +0530

    ID-949 UI Implementation for appstore

--- AppmakerStore/src/app/app.module.ts
+++ AppmakerStore/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppCardComponent } from './app-card/app-card.component';
+import { AppDetailsComponent } from './app-details/app-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppCardComponent,
+    AppDetailsComponent
   ],
   imports: [
     BrowserModule,
