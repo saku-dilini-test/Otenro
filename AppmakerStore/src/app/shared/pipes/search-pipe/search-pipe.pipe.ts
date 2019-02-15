9feebb69455f82c15a9f0ffbfe2faaf8c6616fc0
sudarshanaonbitlabs <sudarshana@simatosolutions.com>
2019-02-15 12:19:48 +0530

    ID-1023 Error message for 0 search results

--- AppmakerStore/src/app/shared/pipes/search-pipe/search-pipe.pipe.ts
+++ AppmakerStore/src/app/shared/pipes/search-pipe/search-pipe.pipe.ts
@@ -2,17 +2,30 @@
  * Created by Sudarshana on 1/09/19.
  */
 import { Pipe, PipeTransform } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Pipe({
   name: 'searchPipe'
 })
 export class SearchPipePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    if (!value || !args) {
-      return value;
-    }
-    return value.filter(item => item.appName.toLocaleLowerCase().indexOf(args.appName) !== -1);
-  }
+  constructor(private toastr: ToastrService) {}
 
+  transform(value: any, args?: any): any {
+
+    let pipeResults: any;
+    if (!value || !args) {
+
+      pipeResults = value;
+    } else {
+      pipeResults = value.filter(item => item.appName.toLocaleLowerCase().indexOf(args.appName) !== -1);
+
+      if (pipeResults.length === 0) {
+        setTimeout(() => {
+          this.toastr.error('', `Sorry, we couldn't find any apps that matched your search term`);
+        }, 100);
+      }
+    }
+    return pipeResults;
+  }
 }
